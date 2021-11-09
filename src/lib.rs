4b2af7629037646686a7a9dbcdd96b8dbcc0629c
Tijl Leenders <tijl.leenders@gmail.com>
2021-11-09 11:14:51 +0100

    my first Rust struct: a Goal

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,41 @@
+use uuid::Uuid;
+
+pub struct Goal {
+    id: Uuid,
+    pub title: String,
+}
+
+impl Goal {
+    /// Construct a new default Goal
+    ///
+    /// # Example
+    /// ```
+    /// # use zinzen_scheduler::Goal;
+    /// let goal : Goal = Goal::new();
+    ///
+    /// assert_eq!(
+    ///     goal.title,
+    ///     String::from("test")
+    /// );
+    /// ```
+    pub fn new() -> Goal {
+        Goal {
+            id: Uuid::new_v4(),
+            title: String::from("test"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works2() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use zinzen_scheduler;
+
+fn main() {
+    let test_goal = zinzen_scheduler::Goal::new();
+    print!("title:{}\n", test_goal.title)
+}
