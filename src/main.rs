2bdbd0652681f6f52b050c37fe70a268edb807f9
Tijl Leenders <tijl.leenders@gmail.com>
2021-11-09 11:41:58 +0100

    add DateTime package

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use chrono::{DateTime, Utc};
 use zinzen_scheduler;
 
 fn main() {
     let test_goal = zinzen_scheduler::Goal::new();
-    print!("title:{}\n", test_goal.title)
+    print!("title:{}\n", test_goal.title);
+
+    let now: DateTime<Utc> = Utc::now();
+
+    println!("UTC now is: {}", now);
+    println!("UTC now in RFC 2822 is: {}", now.to_rfc2822());
+    println!("UTC now in RFC 3339 is: {}", now.to_rfc3339());
+    println!(
+        "UTC now in a custom format is: {}",
+        now.format("%a %b %e %T %Y")
+    );
 }
