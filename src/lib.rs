92fb197b7f07ed41ab75d4ef7177ded5e06a8bd1
Tijl Leenders <tijl.leenders@gmail.com>
2021-11-16 09:42:37 +0100

    add test script + default include wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 use log;
 use std::str::FromStr;
 use std::{fmt, usize};
+use wasm_bindgen::prelude::*;
+
+// Import the `window.alert` function from the Web.
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+// Export a `greet` function from Rust to JavaScript, that alerts a
+// hello message.
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
 
 #[derive(Debug)]
 enum TaskStatus {
