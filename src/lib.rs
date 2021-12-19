0446f884eb88ea377af6410753cd9de3d44a271f
Tijl Leenders <tijl.leenders@gmail.com>
2021-12-19 06:18:37 +0100

    test for issue: all slots having same task_id

--- src/lib.rs
+++ src/lib.rs
@@ -478,6 +478,81 @@ mod tests {
     }
 
     #[test]
+    fn two_goals() {
+        init_env_logger();
+
+        let mut calendar = Calendar {
+            max_time_units: 720,
+            time_unit_qualifier: String::from("h"),
+            tasks: Vec::new(),
+            slots: Vec::new(),
+        };
+
+        let task1 = Task {
+            duration_scheduled: 0,
+            duration_to_schedule: 1,
+            task_id: 1,
+            task_status: TaskStatus::UNSCHEDULED,
+            goal_id: "5f39a726-641c-4c1a-aa54-2f28a3847ee8".to_string(),
+        };
+        let task2 = Task {
+            task_id: 2,
+            duration_to_schedule: 1,
+            duration_scheduled: 0,
+            task_status: TaskStatus::UNSCHEDULED,
+            goal_id: "".to_string(),
+        };
+
+        let slot1 = Slot {
+            task_id: 1,
+            begin: 7,
+            end: 720,
+        };
+        let slot2 = Slot {
+            task_id: 2,
+            begin: 7,
+            end: 720,
+        };
+
+        calendar.tasks.push(task1);
+        calendar.tasks.push(task2);
+
+        calendar.slots.push(slot1);
+        calendar.slots.push(slot2);
+
+        #[cfg(not(target_arch = "wasm32"))]
+        log::info!("Calendar:{:#?}\n", calendar);
+
+        calendar.schedule();
+
+        #[cfg(not(target_arch = "wasm32"))]
+        log::info!("Calendar:{:#?}\n", calendar);
+
+        assert_eq!(720, calendar.max_time_units);
+        assert_eq!("h", calendar.time_unit_qualifier);
+        let mut s_vec: Vec<Slot> = Vec::new();
+        let expected_slot1 = Slot {
+            task_id: 1,
+            begin: 4,
+            end: 5,
+        };
+        let expected_slot2 = Slot {
+            task_id: 2,
+            begin: 24,
+            end: 25,
+        };
+        let expected_slot3 = Slot {
+            task_id: 3,
+            begin: 48,
+            end: 49,
+        };
+        s_vec.push(expected_slot1);
+        s_vec.push(expected_slot2);
+        s_vec.push(expected_slot3);
+        assert_eq!(s_vec, calendar.slots);
+    }
+
+    #[test]
     fn test_find_cut_off_type() {
         init_env_logger();
         let slot = Slot {
