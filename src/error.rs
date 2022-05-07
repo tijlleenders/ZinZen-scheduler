c1765df29c4016038e703a4fb64161dba19d459a
sokorototo <nyachiengatoto@gmail.com>
2022-05-07 17:07:41 +0300

    .

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,6 @@ extern "C" {
 #[allow(non_upper_case_globals)]
 impl ErrorCode {
 	pub(crate) const IPCDataOverflow: u8 = 2;
-	pub(crate) const DataInIPCNotValidUTF8: u8 = 3;
 	pub(crate) const DeserializationError: u8 = 4;
 	pub(crate) const SerializationError: u8 = 5;
 }
