412ee6a4db41f1820e6dd6529f5ab152c95ab48b
sokorototo <nyachiengatoto@gmail.com>
2022-05-10 01:28:17 +0300

    Default value for Task

--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use time::PrimitiveDateTime;
 
+use crate::scheduler::Schedule;
+
 /// A [Task] is an item a user is expected to accomplish, it is simply a time-slice in a user's schedule.
 /// Through many tasks can a user achieve a
 #[derive(Serialize, Deserialize, Debug)]
@@ -11,4 +13,19 @@ pub struct Task {
 	pub(crate) start: PrimitiveDateTime,
 	/// When this task ends
 	pub(crate) finish: PrimitiveDateTime,
+	/// A Tasks flexibility is how flexible it is within it's allocated time frame.
+	pub(crate) flexibility: f64,
+}
+
+impl Task {
+	pub(crate) fn fill(schedule: &mut Schedule) -> Self {
+		let max_seconds = (schedule.timeline.1 - schedule.timeline.0).as_seconds_f64().abs();
+
+		Self {
+			goal_id: 0,
+			start: schedule.timeline.0,
+			finish: schedule.timeline.1,
+			flexibility: max_seconds / (3600.0),
+		}
+	}
 }
