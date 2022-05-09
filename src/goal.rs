e815ce6dcd2a048f09ffe5fc64a4d587ccad14c6
sokorototo <nyachiengatoto@gmail.com>
2022-05-10 01:27:22 +0300

    Rewrote Goal to match wiki definition

--- src/goal.rs
+++ src/goal.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroUsize;
+
 use crate::{console, error::ErrorCode, IPC_BUFFER};
 use serde::{Deserialize, Serialize};
 
@@ -16,16 +18,17 @@ pub unsafe fn load_goals_from_ipc(ipc_offset: usize) -> Vec<Goal> {
 #[non_exhaustive]
 pub struct Goal {
 	/// Every goal has a unique ID
-	pub id: usize,
+	pub id: NonZeroUsize,
 	/// A goal's description
 	pub description: String,
-	/// How much total time should a user put into their goal, eg "I want to learn how to code, and I want to dedicate 72 hours of work"
-	pub duration: time::Duration,
+	/// How much total time should a user put into their goal, eg "I want to learn how to code, and I want to code 6 hours per day"
+	pub task_duration: time::Duration,
 	/// The interval between a Goal's tasks, this can be used to repeat a Goal daily, weekly, etc
-	pub interval: time::Duration,
-	/// Allows the user to set exact times for when a task should be done, given as a time of day and an interval in hours
-	/// Here `interval` is fundamentally always divisible by 24
-	pub time_constraint: Option<time::Time>,
+	/// Here `interval` is fundamentally always divisible by 24.
+	/// NONE means it happens only once
+	pub interval: Option<time::Duration>,
+	/// Allows the user to set exact times for when a task should be start, given as a date and time
+	pub time_constraint: Option<time::PrimitiveDateTime>,
 	/// Where each task should be committed to, eg "I want to cook for my dog at home".
 	/// This is useful to make sure a schedule makes sense, since people can't teleport from place to place in minutes
 	pub location_constraint: Option<usize>,
@@ -34,10 +37,10 @@ pub struct Goal {
 impl Default for Goal {
 	fn default() -> Self {
 		Self {
-			id: 0,
+			id: unsafe { NonZeroUsize::new_unchecked(255) },
 			description: "[NO DESCRIPTION]".to_string(),
-			duration: time::Duration::ZERO,
-			interval: time::Duration::DAY,
+			task_duration: time::Duration::ZERO,
+			interval: None,
 			time_constraint: (None),
 			location_constraint: None,
 		}
@@ -52,12 +55,14 @@ impl Goal {
 		task_allocation_b: time::Duration,
 	) -> bool {
 		// No constraint, no intersection
-		if let (Some(time_a), Some(time_b)) = (self.time_constraint, other.time_constraint) {
+		if let (Some(interval), Some(time_a), Some(time_b)) =
+			(self.interval, self.time_constraint, other.time_constraint)
+		{
 			// A intersects B, if time_alloc of A enters into interval of B
 			let a_intersects_b = (time_a - time_b).abs() < task_allocation_a;
 
 			// B intersects A, if time_alloc of B enters into interval of A
-			let b_intersects_a = time_b + task_allocation_b > time_a + self.interval;
+			let b_intersects_a = time_b + task_allocation_b > time_a + interval;
 
 			a_intersects_b || b_intersects_a
 		} else {
