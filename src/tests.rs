f98bc4ea2b387214039661e83769eaac1542801f
sokorototo <nyachiengatoto@gmail.com>
2022-05-18 23:56:15 +0300

    Added new test for debugging

--- src/tests.rs
+++ src/tests.rs
@@ -68,7 +68,7 @@ pub(crate) fn test_scheduler() {
 	use crate::scheduler::generate_schedule;
 
 	let date_a = time::Date::from_calendar_date(2019, time::Month::June, 1).unwrap();
-	let date_b = time::Date::from_calendar_date(2019, time::Month::June, 2).unwrap();
+	let date_b = time::Date::from_calendar_date(2019, time::Month::June, 20).unwrap();
 
 	let timeline = (
 		time::PrimitiveDateTime::new(date_a, time::Time::MIDNIGHT),
@@ -113,6 +113,73 @@ pub(crate) fn test_scheduler() {
 		}
 
 		dbg!(task.flexibility, task.goal_id);
+	}
+
+	dbg!(counts);
+}
+
+#[test]
+pub(crate) fn test_scheduler_02() {
+	use crate::scheduler::generate_schedule;
+
+	let date_a = time::Date::from_calendar_date(2019, time::Month::June, 9).unwrap();
+	let date_b = time::Date::from_calendar_date(2019, time::Month::June, 20).unwrap();
+
+	let timeline = (
+		time::PrimitiveDateTime::new(date_a, time::Time::MIDNIGHT),
+		time::PrimitiveDateTime::new(date_b, time::Time::MIDNIGHT),
+	);
+
+	let goals = &mut [
+		Goal {
+			id: NonZeroUsize::new(7).unwrap(),
+			interval: None,
+			task_duration: Duration::hours(12),
+			..Default::default()
+		},
+		Goal {
+			id: NonZeroUsize::new(1).unwrap(),
+			task_duration: Duration::hours(12),
+			interval: Some(Duration::DAY),
+			..Default::default()
+		},
+		Goal {
+			id: NonZeroUsize::new(2).unwrap(),
+			task_duration: Duration::hours(12),
+			interval: Some(Duration::WEEK),
+			..Default::default()
+		},
+		Goal {
+			id: NonZeroUsize::new(3).unwrap(),
+			task_duration: Duration::hours(12),
+			interval: Some(Duration::WEEK * 4f32),
+			..Default::default()
+		},
+		Goal {
+			id: NonZeroUsize::new(4).unwrap(),
+			task_duration: Duration::hours(12),
+			interval: Some(Duration::WEEK * 4f32),
+			..Default::default()
+		},
+		Goal {
+			id: NonZeroUsize::new(5).unwrap(),
+			task_duration: Duration::hours(12),
+			interval: Some(Duration::WEEK * 52f32),
+			..Default::default()
+		},
+	];
+
+	let mut counts = HashMap::new();
+	let slots = generate_schedule(goals, timeline).explode().slots;
+
+	for task in slots {
+		if let Some(count) = counts.get_mut(&task.goal_id) {
+			*count += 1
+		} else {
+			counts.insert(task.goal_id, 1);
+		}
+
+		dbg!(task.goal_id, task.flexibility);
 	}
 
 	dbg!(counts);
