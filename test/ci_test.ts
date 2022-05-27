1830025334e3aa95665128c4ab2ea6e654d722d0
Tijl Leenders <tijl.leenders@gmail.com>
2022-05-27 20:01:43 +0200

    make test case async

--- test/ci_test.ts
+++ test/ci_test.ts
@@ -1,13 +1,14 @@
 import { loadAPI } from "../ts/api.ts";
 //import { assertEquals as _assert } from "https://deno.land/std@0.140.0/testing/asserts.ts";
 
-// Load the API
-const API = loadAPI("https://github.com/tijlleenders/ZinZen-scheduler/raw/main/ts/scheduler.wasm");
-
-// Load JSON
-const json = Deno.readTextFile("test/goals.json");
-
-Deno.test("API.processTaskCount", () => {
+Deno.test("API.processTaskCount", async () => {
+	
+	// Load the API
+	const API = await loadAPI("https://github.com/tijlleenders/ZinZen-scheduler/raw/main/ts/scheduler.wasm");
+	
+	// Load JSON
+	const json = Deno.readTextFile("test/goals.json");
+	
 	Promise.all([API, json]).then(([API, json]) => {
 		console.log("API:", API.generateSchedule)
 		console.log("Output:", json)
