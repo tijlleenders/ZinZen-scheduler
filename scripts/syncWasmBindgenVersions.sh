25dde828f66130ea31757d8872d4d4fea9964199
xy2_ <hugo@xy2.dev>
2022-07-19 12:27:39 +0200

    ci

--- scripts/syncWasmBindgenVersions.sh
+++ scripts/syncWasmBindgenVersions.sh
@@ -1,3 +1,6 @@
+WASM_BINDGEN_VERSION=$(wasm-bindgen -V | cut -d " " -f 2)
+
 echo 'Syncing wasm-bindgen version in crate with that of the installed CLI...'
-sed -i "s/^wasm-bindgen\ =.*$/wasm-bindgen = \"=$WASM_BINDGEN_VERSION\"/" Cargo.toml
+# hack, will not work if we have multiple "version = ..." in Cargo.toml
+sed -i "s/^version\ =.*$/version = \"=$WASM_BINDGEN_VERSION\"/" Cargo.toml
 cargo update --package wasm-bindgen
