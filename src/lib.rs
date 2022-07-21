07fc866da1ab27fb6c89de2416a48b3e3735ca89
xy2_ <hugo@xy2.dev>
2022-07-21 19:25:15 +0200

    Merge main

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,36 @@ use wasm_bindgen::prelude::*;
 pub use goal::{Goal, Repetition};
 pub use input::Input;
 
-/// API modules
-mod error;
-mod goal;
+use crate::task_generator::task_generator;
 
 /// Project details
+mod goal;
 mod input;
 mod task;
 mod task_generator;
 mod task_placer;
 mod util;
+
+#[wasm_bindgen]
+extern "C" {
+	// Import console.log from JavaScript
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(s: &str);
+}
+
+/// A little logging function to make it easier to debug, calls console.log
+pub fn console_log<S: AsRef<str>>(input: S) {
+	log(input.as_ref());
+}
+
+/// Not necessarily an entry point, just initializes the console error hook
+#[wasm_bindgen(start)]
+pub fn init() -> Result<(), JsError> {
+	std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+	log("Successfully initialized scheduler, ready to go!");
+
+	Ok(())
+}
 
 #[wasm_bindgen(typescript_custom_section)]
 const TS_APPEND_CONTENT: &'static str = r#"
@@ -25,20 +45,18 @@ interface Input {
 }
 "#;
 
-// https://rustwasm.github.io/wasm-bindgen/reference/arbitrary-data-with-serde.html
+/// Generates a task and slots list from the provided parameters
 #[wasm_bindgen]
-pub fn schedule(input: JsValue) -> JsValue {
-	use task_generator::task_generator;
-
-	// Set console error hook, so we get console errors if this panic. This is only ran once
-	console_error_panic_hook::set_once();
-
-	// TODO serde error handling
-	let input = input.into_serde().unwrap();
+pub fn schedule(input: JsValue) -> Result<JsValue, JsError> {
+	// JsError implements From<Error>, so we can just use `?` on any Error
+	let input = input.into_serde()?;
 
 	let placer = task_generator(input);
-	let result = placer.task_placer();
+	let result = match placer.task_placer() {
+		Ok(res) => res,
+		// How tedious, these error types are somehow incomaptible
+		Err(err) => return Err(JsError::new(&err.to_string())),
+	};
 
-	// Any errors from unwrap() here is our fault
-	JsValue::from_serde(&result).unwrap()
+	Ok(JsValue::from_serde(&result)?)
 }
