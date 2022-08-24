fc56bf8303d37168db4f8235f391d1d1664e0b70
Eric Githinji <ericgithinji@gmail.com>
2022-08-24 21:34:27 +0300

    Add methods to Task.
    These methods will be called by the task_processor and make it easier to
    modify individual tasks.

--- src/task.rs
+++ src/task.rs
@@ -1,20 +1,22 @@
-use chrono::{NaiveDateTime, Duration};
+use chrono::{Duration, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
 
 /// One or many created from a Goal by the preprocessor.
 /// To be scheduled in order by the scheduler.
-#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[derive(Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct Task {
 	pub id: usize,
 	pub goal_id: usize,
-    pub title: String,
+	pub title: String,
 	pub duration: usize,
 	pub status: TaskStatus,
 	pub flexibility: usize,
 	pub start: NaiveDateTime,
-	pub end: NaiveDateTime,
+	pub deadline: NaiveDateTime,
 	pub slots: Vec<(NaiveDateTime, NaiveDateTime)>,
+	pub confirmed_start: Option<NaiveDateTime>,
+	pub confirmed_deadline: Option<NaiveDateTime>,
 }
 
 impl Ord for Task {
@@ -30,28 +32,57 @@ impl PartialOrd for Task {
 }
 
 impl Task {
-	pub fn new(id: usize, goal_id: usize, title: String, duration: usize, start: NaiveDateTime, end: NaiveDateTime) -> Self {
+	pub fn new(
+		id: usize,
+		goal_id: usize,
+		title: String,
+		duration: usize,
+		start: NaiveDateTime,
+		deadline: NaiveDateTime,
+	) -> Self {
 		Self {
 			id,
 			goal_id,
-            title,
+			title,
 			duration,
 			status: TaskStatus::UNSCHEDULED,
 			flexibility: 0,
 			start,
-			end,
+			deadline,
 			slots: Vec::new(),
+			confirmed_start: None,
+			confirmed_deadline: None,
 		}
 	}
 
-    pub fn calculate_flexibility(&mut self) {
-        let duration_available = self.slots[self.slots.len()-1].1 - self.slots[0].0;
-        let hours_available = duration_available.num_hours() as usize;
-        self.flexibility = hours_available - self.duration + 1;
+	pub fn calculate_flexibility(&mut self) {
+		let duration_available = self.slots[self.slots.len() - 1].1 - self.slots[0].0;
+		let hours_available = duration_available.num_hours() as usize;
+		self.flexibility = hours_available - self.duration + 1;
+	}
+
+	pub fn set_confirmed_start(&mut self, start: NaiveDateTime) {
+		self.confirmed_start = Some(start);
+	}
+
+	pub fn set_confirmed_deadline(&mut self, deadline: NaiveDateTime) {
+		self.confirmed_deadline = Some(deadline);
+	}
+
+    pub fn get_slots(&self) -> Vec<(NaiveDateTime,NaiveDateTime)> {
+        self.slots.clone()
+    }
+
+    pub fn remove_slot(&mut self, slot: &(NaiveDateTime,NaiveDateTime)) {
+        for i in 0..self.slots.len() {
+            if &self.slots[i] == slot {
+                self.slots.remove(i);
+            }
+        }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub enum TaskStatus {
 	UNSCHEDULED,
 	SCHEDULED,
