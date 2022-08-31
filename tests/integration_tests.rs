aa4616da4d61b852f01108f763735f181f481c46
Eric Githinji <ericgithinji@gmail.com>
2022-08-31 13:03:30 +0300

    Addresses issue #52. Add new test.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -44,3 +44,17 @@ fn repetition_daily_bounds_works() {
 		desired_output
 	);
 }
+
+#[test]
+fn non_repetitive_bounds_multipledays_works() {
+    let input_path = Path::new("./js-tests/non-rep-bounds-multipledays/input.json");
+	let output_path = Path::new("./js-tests/non-rep-bounds-multipledays/output.json");
+	let input: Input = common::get_input_from_json(input_path).unwrap();
+	let desired_output: String = common::get_output_string_from_json(output_path).unwrap();
+
+	let output: Vec<Output> = scheduler::run_scheduler(input);
+	assert_eq!(
+		serde_json::to_string(&output).unwrap(),
+		desired_output
+	);
+}
