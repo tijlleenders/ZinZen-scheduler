cbe109e2f23950565cc703aa9585007e0cecc0e5
Eric Githinji <ericgithinji@gmail.com>
2022-09-06 14:08:18 +0300

    Fix wednesdays test as a result of last commit (893a0f2593cc8904fbd52991c25702be11a9a60a)

--- js-tests/every-wednesday.ts
+++ js-tests/every-wednesday.ts
@@ -20,14 +20,7 @@ Deno.test("every wednesday", () => {
         }
       ),
      [
-      {
-        "taskid": 10,
-        "goalid": 1,
-        "title": "piano practice",
-        "duration": 2,
-        "start": "2022-09-07T12:00:00",
-        "deadline": "2022-09-07T14:00:00"
-      },
+      
       {
         "taskid": 13,
         "goalid": 1,
@@ -51,6 +44,14 @@ Deno.test("every wednesday", () => {
         "duration": 2,
         "start": "2022-09-14T12:00:00",
         "deadline": "2022-09-14T14:00:00"
+      },
+      {
+        "taskid": 10,
+        "goalid": 1,
+        "title": "piano practice",
+        "duration": 2,
+        "start": "2022-09-07T12:00:00",
+        "deadline": "2022-09-07T14:00:00"
       }
     ]      
   );
