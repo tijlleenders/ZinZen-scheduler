bda819f269a52be5ccac8a1182612226d2dab36e
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 11:14:21 +0300

    Apply some formatting with rustfmt.

--- src/task_placer.rs
+++ src/task_placer.rs
@@ -6,7 +6,7 @@
 
 use crate::task::Task;
 use crate::task::TaskStatus::{IMPOSSIBLE, SCHEDULED};
-use crate::time_slice_iterator::{TimeSliceIterator, Repetition};
+use crate::time_slice_iterator::{Repetition, TimeSliceIterator};
 use chrono::{Duration, NaiveDateTime, Timelike};
 
 pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, calendar_end: NaiveDateTime) -> Vec<Task> {
@@ -19,70 +19,72 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 	let time_slots: Vec<(NaiveDateTime, NaiveDateTime)> = time_slice_iterator.collect();
 
 	//slides 2 - 7 (assign slots to tasks)
-    for task in tasks.iter_mut() {
-        let mut i = 0;
-        while i < time_slots.len() {
-           //check if the time_slot is:
-           //1) within the start and deadline dates of the task
-            if (time_slots[i].0 >= task.start) && (time_slots[i].1 < task.deadline) {
-                //2) after the after_time of the task
-                if time_slots[i].0.hour() >= task.after_time as u32 {
-                    for _ in 0..(get_num_slots(task)) as usize {
-                        if i < time_slots.len() {
-                            task.slots.push(time_slots[i]);
-                            i = i + 1;
-                        }
-                    }
-                    //skip to midnight so as not to add more slots on the same day
-                    while time_slots[i-1].1.hour() != 0  {
-                        i = i + 1;
-                        if i == time_slots.len() {break;}
-                    }
-                    //if the remaining slots on calendar are less than this task's duration,
-                    //truncate the task's duration
-                    if task.slots.len() < task.duration {
-                        task.duration = task.slots.len();
-                    }
-                    continue;
-               }
-           }
-           i = i + 1;
-        }
-        task.calculate_flexibility();
-    }
+	for task in tasks.iter_mut() {
+		let mut i = 0;
+		while i < time_slots.len() {
+			//check if the time_slot is:
+			//1) within the start and deadline dates of the task
+			if (time_slots[i].0 >= task.start) && (time_slots[i].1 < task.deadline) {
+				//2) after the after_time of the task
+				if time_slots[i].0.hour() >= task.after_time as u32 {
+					for _ in 0..(get_num_slots(task)) as usize {
+						if i < time_slots.len() {
+							task.slots.push(time_slots[i]);
+							i = i + 1;
+						}
+					}
+					//skip to midnight so as not to add more slots on the same day
+					while time_slots[i - 1].1.hour() != 0 {
+						i = i + 1;
+						if i == time_slots.len() {
+							break;
+						}
+					}
+					//if the remaining slots on calendar are less than this task's duration,
+					//truncate the task's duration
+					if task.slots.len() < task.duration {
+						task.duration = task.slots.len();
+					}
+					continue;
+				}
+			}
+			i = i + 1;
+		}
+		task.calculate_flexibility();
+	}
 
 	tasks.sort();
 	tasks.reverse();
 	let mut scheduled_tasks = Vec::new();
 
-    //slide 9 (assign slot(s) to task with flexibilityof 1)
-    for index in 0..tasks.len() {
-        if tasks[index].flexibility == 1 {
-            let my_slots = tasks[index].get_slots();
-            tasks[index].set_confirmed_start(my_slots[0].0);
-            let deadline = my_slots[my_slots.len() - 1].1;
-            tasks[index].set_confirmed_deadline(deadline);
-            tasks[index].status = SCHEDULED;
-            scheduled_tasks.push(tasks[index].clone());
-            //slide 10 (remove the assigned slot from other tasks' slot lists)
-            for task in &mut tasks {
-                for i in 0..my_slots.len() {
-                    if task.slots.contains(&my_slots[i]) {
-                        task.remove_slot(&my_slots[i]);
-                    }
-                }
-            }
-        }
-    }
+	//slide 9 (assign slot(s) to task with flexibilityof 1)
+	for index in 0..tasks.len() {
+		if tasks[index].flexibility == 1 {
+			let my_slots = tasks[index].get_slots();
+			tasks[index].set_confirmed_start(my_slots[0].0);
+			let deadline = my_slots[my_slots.len() - 1].1;
+			tasks[index].set_confirmed_deadline(deadline);
+			tasks[index].status = SCHEDULED;
+			scheduled_tasks.push(tasks[index].clone());
+			//slide 10 (remove the assigned slot from other tasks' slot lists)
+			for task in &mut tasks {
+				for i in 0..my_slots.len() {
+					if task.slots.contains(&my_slots[i]) {
+						task.remove_slot(&my_slots[i]);
+					}
+				}
+			}
+		}
+	}
 
 	//slides 12-20 (attempt to schedule the other tasks without conflicting with other tasks'
-    //slots)
-    while tasks.len() > 0 {
+	//slots)
+	while tasks.len() > 0 {
 		let mut task = tasks.remove(0);
-		'outer: for (index,_) in task.get_slots().iter().enumerate() {
-            let my_slots = task.get_slots();
-            let desired_first_slot = my_slots.get(index).unwrap();
-            let desired_last_slot = my_slots.get(index + task.duration - 1).unwrap();
+		'outer: for (index, _) in task.get_slots().iter().enumerate() {
+			let my_slots = task.get_slots();
+			let desired_first_slot = my_slots.get(index).unwrap();
+			let desired_last_slot = my_slots.get(index + task.duration - 1).unwrap();
 			for other_task in tasks.iter() {
 				if other_task.status == SCHEDULED {
 					continue;
@@ -102,9 +104,9 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 }
 
 fn get_num_slots(task: &Task) -> usize {
-    if task.before_time > task.after_time {
-        task.before_time - task.after_time
-    } else {
-        task.before_time + (24-task.after_time)
-    }
+	if task.before_time > task.after_time {
+		task.before_time - task.after_time
+	} else {
+		task.before_time + (24 - task.after_time)
+	}
 }
