a3065a87047d4aafcc11fa6a69f843033e2b3a67
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 11:11:18 +0300

    Refactor task placer.
    This is to handle tasks where the beforetime is on the following day
    e.g. sleeping.

--- src/task_placer.rs
+++ src/task_placer.rs
@@ -20,19 +20,33 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 
 	//slides 2 - 7 (assign slots to tasks)
     for task in tasks.iter_mut() {
-        for i in 0..time_slots.len() {
-           //check if the time_slot is 1) within the start and deadline dates of the task, 2)
-           //within the after_time and before_time of the task, and 3) the task doesn't already
-           //contain the slot
-           if (time_slots[i].0 >= task.start) && (time_slots[i].1 < task.deadline) {
-               if (time_slots[i].0.hour() >= task.after_time as u32) && (time_slots[i].1.hour() <= task.before_time as u32) {
-                    if !task.slots.contains(&time_slots[i]){
-                        for j in 0..(task.before_time-task.after_time) as usize {
-                            task.slots.push(time_slots[i+j]);
+        let mut i = 0;
+        while i < time_slots.len() {
+           //check if the time_slot is:
+           //1) within the start and deadline dates of the task
+            if (time_slots[i].0 >= task.start) && (time_slots[i].1 < task.deadline) {
+                //2) after the after_time of the task
+                if time_slots[i].0.hour() >= task.after_time as u32 {
+                    for _ in 0..(get_num_slots(task)) as usize {
+                        if i < time_slots.len() {
+                            task.slots.push(time_slots[i]);
+                            i = i + 1;
                         }
                     }
+                    //skip to midnight so as not to add more slots on the same day
+                    while time_slots[i-1].1.hour() != 0  {
+                        i = i + 1;
+                        if i == time_slots.len() {break;}
+                    }
+                    //if the remaining slots on calendar are less than this task's duration,
+                    //truncate the task's duration
+                    if task.slots.len() < task.duration {
+                        task.duration = task.slots.len();
+                    }
+                    continue;
                }
            }
+           i = i + 1;
         }
         task.calculate_flexibility();
     }
@@ -42,7 +56,6 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 	let mut scheduled_tasks = Vec::new();
 
     //slide 9 (assign slot(s) to task with flexibilityof 1)
-	//TODO: need to make this more concise
     for index in 0..tasks.len() {
         if tasks[index].flexibility == 1 {
             let my_slots = tasks[index].get_slots();
@@ -50,7 +63,6 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
             let deadline = my_slots[my_slots.len() - 1].1;
             tasks[index].set_confirmed_deadline(deadline);
             tasks[index].status = SCHEDULED;
-            println!("Scheduled {}",tasks[index].title);
             scheduled_tasks.push(tasks[index].clone());
             //slide 10 (remove the assigned slot from other tasks' slot lists)
             for task in &mut tasks {
@@ -65,9 +77,9 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 
 	//slides 12-20 (attempt to schedule the other tasks without conflicting with other tasks'
     //slots)
-	while tasks.len() > 0 {
+    while tasks.len() > 0 {
 		let mut task = tasks.remove(0);
-		'outer: for (index,slot) in task.get_slots().iter().enumerate() {
+		'outer: for (index,_) in task.get_slots().iter().enumerate() {
             let my_slots = task.get_slots();
             let desired_first_slot = my_slots.get(index).unwrap();
             let desired_last_slot = my_slots.get(index + task.duration - 1).unwrap();
@@ -87,4 +99,12 @@ pub fn task_placer<'a>(mut tasks: Vec<Task>, calendar_start: NaiveDateTime, cale
 		}
 	}
 	scheduled_tasks
+}
+
+fn get_num_slots(task: &Task) -> usize {
+    if task.before_time > task.after_time {
+        task.before_time - task.after_time
+    } else {
+        task.before_time + (24-task.after_time)
+    }
 }
