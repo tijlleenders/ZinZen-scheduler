487c1859c96e272f448b448d9b7311128c26450c
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 14:20:18 +0300

    Make deno test use common input/output jsons.

--- js-tests/repetition.ts
+++ js-tests/repetition.ts
@@ -5,95 +5,17 @@ import {
 } from "https://deno.land/std@0.141.0/testing/asserts.ts";
 import input from "./repetition-daily/input.json" assert {type: "json"};
 import output from "./repetition-daily/output.json" assert {type: "json"};
+import input2 from "./repetition-daily-bounds/input.json" assert {type: "json"};
+import output2 from "./repetition-daily-bounds/output.json" assert {type: "json"};
 
 Deno.test("can duplicate with daily repetition", () => {
   assertEquals(
-    schedule({
-      "startDate": "2022-01-01T00:00:00",
-      "endDate": "2022-01-04T00:00:00",
-      "goals": [
-        {
-          "id": 1,
-          "title": "walk",
-          "duration": 1,
-          "repetition": "daily",
-        },
-      ],
-    }),
-     [
-      {
-        "taskid": 12,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-03T00:00:00",
-        "deadline": "2022-01-03T01:00:00",
-      },
-      {
-        "taskid": 11,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-02T00:00:00",
-        "deadline": "2022-01-02T01:00:00",
-      },
-      {
-        "taskid": 10,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-01T00:00:00",
-        "deadline": "2022-01-01T01:00:00",
-      },
-     ] 
-  );
+    schedule(input),output);
 });
 
 Deno.test("daily repetition with daily bounds", () => {
   assertEquals(
-    schedule({
-      "startDate": "2022-01-01T00:00:00",
-      "endDate": "2022-01-04T00:00:00",
-      "goals": [
-        {
-          "id": 1,
-          "title": "walk",
-          "duration": 1,
-          "repetition": "daily",
-          "start": "2022-01-01T00:00:00",
-          "deadline": "2022-01-04T00:00:00",
-          "after_time": 10,
-          "before_time": 16
-        },
-      ],
-    }),
-    [
-      {
-        "taskid": 12,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-03T10:00:00",
-        "deadline": "2022-01-03T11:00:00",
-      },
-      {
-        "taskid": 11,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-02T10:00:00",
-        "deadline": "2022-01-02T11:00:00",
-      },
-      {
-        "taskid": 10,
-        "goalid": 1,
-        "title": "walk",
-        "duration": 1,
-        "start": "2022-01-01T10:00:00",
-        "deadline": "2022-01-01T11:00:00",
-      },
-    ],
-  );
+    schedule(input2),output2);
 });
 
 // TODO should check the error for this test, see issue #26,
