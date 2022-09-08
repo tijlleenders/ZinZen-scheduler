807dd8b61a3901b2b96997df9b083cf5d54cf282
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 10:58:56 +0300

    Handle beforetimes spanning to next day.
    There are also some formatting changes with indents and spacing applied
    by rustfmt.

--- src/goal.rs
+++ src/goal.rs
@@ -1,11 +1,9 @@
 use crate::task::Task;
-use crate::time_slice_iterator::{TimeSliceIterator, Repetition};
+use crate::time_slice_iterator::{Repetition, TimeSliceIterator};
 use chrono::Duration;
 use chrono::NaiveDateTime;
 use serde::Deserialize;
 use std::option::Option;
-
-
 
 #[derive(Deserialize, Debug, Default)]
 pub struct Goal {
@@ -20,12 +18,12 @@ pub struct Goal {
 	/// deadline date bound for this Goal's Tasks
 	#[serde(default)]
 	pub deadline: Option<NaiveDateTime>,
-    /// start time bound after which activity should be done
-    #[serde(default)]
-    pub after_time: Option<usize>,
-    /// deadline time bound before which activity should be done
-    #[serde(default)]
-    pub before_time: Option<usize>,
+	/// start time bound after which activity should be done
+	#[serde(default)]
+	pub after_time: Option<usize>,
+	/// deadline time bound before which activity should be done
+	#[serde(default)]
+	pub before_time: Option<usize>,
 }
 
 //#[cfg(test)]
@@ -58,21 +56,21 @@ impl Goal {
 		self
 	}
 
-    pub fn after_time(mut self, after_time: usize) -> Self {
-        self.after_time = Some(after_time);
-        self
-    }
+	pub fn after_time(mut self, after_time: usize) -> Self {
+		self.after_time = Some(after_time);
+		self
+	}
 
-    pub fn before_time(mut self, before_time: usize) -> Self {
-        self.before_time = Some(before_time);
-        self
-    }
+	pub fn before_time(mut self, before_time: usize) -> Self {
+		self.before_time = Some(before_time);
+		self
+	}
 
 	pub fn generate_tasks(self, calendar_start: NaiveDateTime, calendar_end: NaiveDateTime) -> Vec<Task> {
 		let mut tasks = Vec::new();
-        //If there is a repetion in the goal, a different task will be generated for each day of the repetition.
-        //e.g. if the repetition is DAILY, a different task will be generated for each day.
-        //e.g. if the repetition is MONDAYS, a different task will be generated for each monday.
+		//If there is a repetion in the goal, a different task will be generated for each day of the repetition.
+		//e.g. if the repetition is DAILY, a different task will be generated for each day.
+		//e.g. if the repetition is MONDAYS, a different task will be generated for each monday.
 		match self.repetition {
 			Some(rep) => {
 				let time_slices = TimeSliceIterator {
@@ -81,24 +79,29 @@ impl Goal {
 					repetition: rep,
 				};
 				let mut id = 0;
-				for (start,deadline) in time_slices {
+				for (start, deadline) in time_slices {
 					let task_id = format!("{}{}", self.id, id);
+					let deadline = if self.before_time.unwrap_or(24) < self.after_time.unwrap_or(0) {
+						deadline + Duration::days(1)
+					} else {
+						deadline
+					};
 					let t = Task::new(
 						task_id.parse::<usize>().unwrap(),
 						self.id,
 						self.title.clone(),
 						self.duration,
-                        start,
-                        deadline,
-                        self.after_time.unwrap_or(0), 
-                        self.before_time.unwrap_or(24),
+						start,
+						deadline,
+						self.after_time.unwrap_or(0),
+						self.before_time.unwrap_or(24),
 					);
 					tasks.push(t);
 					id = id + 1;
 				}
 			}
-            //If there is no repetition, the task's start and deadline are equivalent to the goal's
-            //start/deadline.
+			//If there is no repetition, the task's start and deadline are equivalent to the goal's
+			//start/deadline.
 			None => {
 				let task_id = format!("{}{}", self.id, 0);
 				let t = Task::new(
@@ -108,8 +111,8 @@ impl Goal {
 					self.duration,
 					self.start.unwrap_or(calendar_start),
 					self.deadline.unwrap_or(calendar_end),
-                    self.after_time.unwrap_or(0),
-                    self.before_time.unwrap_or(24),
+					self.after_time.unwrap_or(0),
+					self.before_time.unwrap_or(24),
 				);
 				tasks.push(t);
 			}
