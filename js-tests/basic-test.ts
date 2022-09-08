9ea6d4800d5c8c98bb9126e67013c4419fa928e8
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 13:46:39 +0300

    Make deno test use common input/output jsons.

--- js-tests/basic-test.ts
+++ js-tests/basic-test.ts
@@ -1,66 +1,9 @@
 import { schedule } from "../js-api/scheduler.js";
 import { assertEquals } from "https://deno.land/std@0.141.0/testing/asserts.ts";
+import input from "./basic-test/input.json" assert {type: "json"};
+import output from "./basic-test/output.json" assert {type: "json"};
 
 Deno.test("basic test from issue #3 (https://github.com/tijlleenders/ZinZen-scheduler/issues/3)", () => {
   assertEquals(
-    schedule({
-      "startDate": "2022-01-01T00:00:00",
-      "endDate": "2022-01-02T00:00:00",
-      "goals": [
-        {
-          "id": 1,
-          "title": "shopping",
-          "duration": 1,
-          "start": "2022-01-01T00:00:00",
-          "deadline": "2022-01-02T00:00:00",
-          "after_time": 10,
-          "before_time": 13
-        },
-        {
-          "id": 2,
-          "title": "dentist",
-          "duration": 1,
-          "start": "2022-01-01T00:00:00",
-          "deadline": "2022-01-02T00:00:00",
-          "after_time": 10,
-          "before_time": 11
-        },
-        {
-          "id": 3,
-          "title": "exercise",
-          "duration": 1,
-          "start": "2022-01-01T00:00:00",
-          "deadline": "2022-01-02T00:00:00",
-          "after_time": 10,
-          "before_time": 18
-        },
-      ],
-    }),
-    [
-      {
-        "taskid": 20,
-        "goalid": 2,
-        "title": "dentist",
-        "duration": 1,
-        "start": "2022-01-01T10:00:00",
-        "deadline": "2022-01-01T11:00:00",
-      },
-      {
-        "taskid": 30,
-        "goalid": 3,
-        "title": "exercise",
-        "duration": 1,
-        "start": "2022-01-01T13:00:00",
-        "deadline": "2022-01-01T14:00:00",
-      },
-      {
-        "taskid": 10,
-        "goalid": 1,
-        "title": "shopping",
-        "duration": 1,
-        "start": "2022-01-01T11:00:00",
-        "deadline": "2022-01-01T12:00:00",
-      },
-    ],
-  );
+    schedule(input),output);
 });
