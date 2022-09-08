a36025985fefb15303d7f94471d3ed45264ceb70
Eric Githinji <ericgithinji@gmail.com>
2022-09-08 15:07:30 +0300

    Apply rustfmt.

--- src/task.rs
+++ src/task.rs
@@ -1,7 +1,7 @@
+use crate::goal::Goal;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
-use crate::goal::Goal;
 
 /// One or many created from a Goal.
 /// To be scheduled in order by the scheduler.
@@ -15,8 +15,8 @@ pub struct Task {
 	pub flexibility: usize,
 	pub start: NaiveDateTime,
 	pub deadline: NaiveDateTime,
-    pub after_time: usize,
-    pub before_time: usize,
+	pub after_time: usize,
+	pub before_time: usize,
 	pub slots: Vec<(NaiveDateTime, NaiveDateTime)>,
 	pub confirmed_start: Option<NaiveDateTime>,
 	pub confirmed_deadline: Option<NaiveDateTime>,
@@ -35,12 +35,7 @@ impl PartialOrd for Task {
 }
 
 impl Task {
-	pub fn new(
-		id: usize,
-		start: NaiveDateTime,
-		deadline: NaiveDateTime,
-        goal: &Goal,
-	) -> Self {
+	pub fn new(id: usize, start: NaiveDateTime, deadline: NaiveDateTime, goal: &Goal) -> Self {
 		Self {
 			id,
 			goal_id: goal.id,
@@ -50,18 +45,18 @@ impl Task {
 			flexibility: 0,
 			start,
 			deadline,
-            after_time: goal.after_time.unwrap_or(0),
-            before_time: goal.before_time.unwrap_or(24),
+			after_time: goal.after_time.unwrap_or(0),
+			before_time: goal.before_time.unwrap_or(24),
 			slots: Vec::new(),
 			confirmed_start: None,
 			confirmed_deadline: None,
 		}
 	}
 
-    //TODO: The current way this is done may not be entirely accurate for tasks that can be done on
-    //multiple days within certain time bounds.
+	//TODO: The current way this is done may not be entirely accurate for tasks that can be done on
+	//multiple days within certain time bounds.
 	pub fn calculate_flexibility(&mut self) {
-        let hours_available = self.slots.len(); 
+		let hours_available = self.slots.len();
 		self.flexibility = hours_available - self.duration + 1;
 	}
 
