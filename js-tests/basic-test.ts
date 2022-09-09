69a2dbeafe0f13034b10a97845b0ffd031881722
Eric Githinji <ericgithinji@gmail.com>
2022-09-09 15:23:15 +0300

    Issue #68. Move deno tests to one file.

--- js-tests/basic-test.ts
+++
@@ -1,9 +0,0 @@
-import { schedule } from "../js-api/scheduler.js";
-import { assertEquals } from "https://deno.land/std@0.141.0/testing/asserts.ts";
-import input from "./basic-test/input.json" assert {type: "json"};
-import output from "./basic-test/output.json" assert {type: "json"};
-
-Deno.test("basic test from issue #3 (https://github.com/tijlleenders/ZinZen-scheduler/issues/3)", () => {
-  assertEquals(
-    schedule(input),output);
-});
