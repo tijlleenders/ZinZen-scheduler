0c31641f861b3e8bb3e15f898621a8230c2331b2
Eric Githinji <ericgithinji@gmail.com>
2022-09-15 12:20:07 +0300

    Slight changes in ordering of output.
    This is as a result of 01b2f788e1be4bdd4ac18a16bc57c873f23bee91. Not
    pushing tasks into a new vec results in slight changes in the ordering
    of the output, doesn't affect the actual tasks output though.

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -469,7 +469,7 @@ fn goal_generates_single_nonrepetitive_task() {
 
 #[test]
 fn output_formatter_works() {
-    let desired_output = r#"[{"taskid":20,"goalid":2,"title":"dentist","duration":1,"start":"2022-01-01T10:00:00","deadline":"2022-01-01T11:00:00"},{"taskid":30,"goalid":3,"title":"exercise","duration":1,"start":"2022-01-01T13:00:00","deadline":"2022-01-01T14:00:00"},{"taskid":10,"goalid":1,"title":"shopping","duration":1,"start":"2022-01-01T11:00:00","deadline":"2022-01-01T12:00:00"}]"#;
+    let desired_output = r#"[{"taskid":30,"goalid":3,"title":"exercise","duration":1,"start":"2022-01-01T13:00:00","deadline":"2022-01-01T14:00:00"},{"taskid":10,"goalid":1,"title":"shopping","duration":1,"start":"2022-01-01T11:00:00","deadline":"2022-01-01T12:00:00"},{"taskid":20,"goalid":2,"title":"dentist","duration":1,"start":"2022-01-01T10:00:00","deadline":"2022-01-01T11:00:00"}]"#;
 
     let (calendar_start, calendar_end) = get_calendar_bounds();
     let scheduled_tasks = task_placer(get_test_tasks(), calendar_start, calendar_end);
@@ -548,28 +548,29 @@ fn task_placer_slots_tasks_correctly() {
     assert_eq!(scheduled_tasks[0].status, SCHEDULED);
     assert_eq!(scheduled_tasks[1].status, SCHEDULED);
     assert_eq!(scheduled_tasks[2].status, SCHEDULED);
+
     assert_eq!(
         scheduled_tasks[0].confirmed_start.unwrap(),
-        NaiveDate::from_ymd(2022, 1, 1).and_hms(10, 0, 0)
-    );
-    assert_eq!(
-        scheduled_tasks[0].confirmed_deadline.unwrap(),
-        NaiveDate::from_ymd(2022, 1, 1).and_hms(11, 0, 0)
-    );
-    assert_eq!(
-        scheduled_tasks[1].confirmed_start.unwrap(),
         NaiveDate::from_ymd(2022, 1, 1).and_hms(13, 0, 0)
     );
     assert_eq!(
-        scheduled_tasks[1].confirmed_deadline.unwrap(),
+        scheduled_tasks[0].confirmed_deadline.unwrap(),
         NaiveDate::from_ymd(2022, 1, 1).and_hms(14, 0, 0)
     );
     assert_eq!(
-        scheduled_tasks[2].confirmed_start.unwrap(),
+        scheduled_tasks[1].confirmed_start.unwrap(),
         NaiveDate::from_ymd(2022, 1, 1).and_hms(11, 0, 0)
     );
     assert_eq!(
-        scheduled_tasks[2].confirmed_deadline.unwrap(),
+        scheduled_tasks[1].confirmed_deadline.unwrap(),
         NaiveDate::from_ymd(2022, 1, 1).and_hms(12, 0, 0)
+    );
+    assert_eq!(
+        scheduled_tasks[2].confirmed_start.unwrap(),
+        NaiveDate::from_ymd(2022, 1, 1).and_hms(10, 0, 0)
+    );
+    assert_eq!(
+        scheduled_tasks[2].confirmed_deadline.unwrap(),
+        NaiveDate::from_ymd(2022, 1, 1).and_hms(11, 0, 0)
     );
 }
