027941fbe2198c530ca0051a7bb4e9bd7bda8c78
mustafa-eldeeb <darshm466@gmail.com>
2023-01-28 07:53:58 -0500

    clean up

--- src/task_generator.rs
+++ src/task_generator.rs
@@ -1,10 +1,7 @@
-use crate::errors::Error;
-use crate::goal::{handle_hierarchy, Goal, Tag};
-use crate::graph_handler::get_graph_info;
+use crate::goal::{handle_dependency, handle_hierarchy, Goal, Tag};
 use crate::input::Input;
 use crate::task::Task;
-use crate::task_placer::task_placer;
-use crate::{Repetition, DAG, output_formatter};
+use crate::Repetition;
 
 /// # Task Generator
 /// Takes an [Input](../input/index.html) and outputs a vector of Unscheduled [Tasks](../task/index.html).
@@ -17,9 +14,8 @@ pub fn task_generator(
 ) -> Vec<Task> {
     let mut counter: usize = 0;
     let mut tasks = vec![];
-   // let goals = handle_hierarchy(goals);
-    let graph_info = get_graph_info(goals.to_owned());
-    let goals = get_ordered_goals(goals, graph_info);
+    let goals = handle_hierarchy(goals);
+    let goals = handle_dependency(goals);
     for goal in goals {
         if let Some(Repetition::FlexWeekly(min, max)) = goal.repeat {
             //Flex repeat goals are handled as follows:
@@ -56,8 +52,7 @@ pub fn task_generator(
                 tasks.extend(goal.generate_tasks(calendar_start, calendar_end, &mut counter));
             }
         } else {
-            let goals_vec=goal.generate_tasks(calendar_start, calendar_end, &mut counter);
-            println!("{:#?}",goals_vec);
+            let goals_vec = goal.generate_tasks(calendar_start, calendar_end, &mut counter);
             tasks.extend(goals_vec);
         }
     }
@@ -73,48 +68,4 @@ fn get_1_hr_goals(goal: Goal) -> Vec<Goal> {
         goals.push(g);
     }
     goals
-}
-
-pub fn get_ordered_goals(goals: Vec<Goal>, graph_info: Vec<(usize, usize)>) -> Vec<Goal> {
-    let mut ordered_ids = DAG::new(graph_info);
-    ordered_ids.reverse();
-    let mut orderd_goals = vec![];
-    for id in ordered_ids {
-        orderd_goals.push(
-            goals
-                .iter()
-                .find(|&x| x.id.parse::<usize>().unwrap() == id)
-                .unwrap()
-                .clone(),
-        )
-    }
-    orderd_goals
-}
-
-#[test]
-fn test() {
-    use crate::graph_handler::*;
-
-    let input = include_str!("/home/mus/ZinZen-scheduler/tests/jsons/goals-dependency/input.json");
-
-    let mut res: Input = serde_json::from_str(&input).expect("Unable to parse");
-
-    let info = get_graph_info(res.goals.clone());
-
-    let goals = get_ordered_goals(res.goals.to_owned(), info);
-    // println!("{:#?}", goals);
-     res.goals= goals;
-    let tasks=task_generator(res);
-    let (scheduled,impossible) = task_placer(tasks);
-    match output_formatter(scheduled, impossible) {
-        Err(Error::NoConfirmedDate(title, id)) => {
-            panic!("Error with task {title}:{id}. Tasks passed to output formatter should always have a confirmed_start/deadline.");
-        }
-        Err(e) => {
-            panic!("Unexpected error: {:?}", e);
-        }
-        Ok(output) => {println!("scheduled {:#?}", output);}
-    }
-    let x = true;
-    assert!(x, "x wasn't true!");
 }
