03aae73ea92091ea75805cc44bac5ecb03bf364f
mustafa-eldeeb <darshm466@gmail.com>
2023-02-20 01:29:22 +0200

    Generate tests  at compile time

---
+++ build.rs
@@ -0,0 +1,80 @@
+use std::io::Write;
+use std::path::PathBuf;
+
+fn write_test(file: &mut std::fs::File, content: &mut str) -> Result<(), std::io::Error> {
+    write!(file, "{} \n\n", content)?;
+    Ok(())
+}
+
+fn get_test_dirs() -> Result<Vec<PathBuf>, std::io::Error> {
+    let dirs = std::fs::read_dir("./tests/jsons")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, std::io::Error>>()?;
+    Ok(dirs)
+}
+
+fn test_template(test_name: &str, test_dir: &str) -> String {
+    let mut result = vec!["\n#[test]\n".to_string()];
+    result.push(format!("fn {}() {{", test_name));
+    result.push(format!(
+        "\n{}let (actual_output, desired_output) = run_test(\"{}\");",
+        space(),
+        test_dir
+    ));
+    result.push(format!(
+        "\n{}assert_eq!(actual_output, desired_output);\n}}",
+        space()
+    ));
+    result.join("")
+}
+fn space() -> String {
+    "    ".to_string()
+}
+fn get_imports() -> String {
+    let mut result = vec!["extern crate scheduler;".to_string()];
+    result.push("\nmod common;".to_string());
+    result.push("\n#[cfg(test)]".to_string());
+    result.push("\nuse pretty_assertions::assert_eq;".to_string());
+    result.push("\nuse scheduler::{FinalOutput, Input};".to_string());
+    result.push("\nuse std::path::Path;\n\n".to_string());
+    result.join("")
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let out_dir = String::from("./tests/");
+    let mut dirs = get_test_dirs().expect("Unable to read tests directory");
+    dirs.retain(|d| !d.file_name().unwrap().eq("benchmark-tests") && (d.is_dir()));
+
+    let dirs_vec = dirs
+        .iter()
+        .map(|d| d.file_name().unwrap().to_str().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut result = vec!["".to_string()];
+    result.push(get_imports());
+    result.push(get_run_test());
+    for dir in dirs_vec.iter() {
+        result.push(test_template(dir.replace("-", "_").as_str(), dir));
+    }
+
+    let mut rust_tests_file = std::fs::File::create(&format!("{}/rust_tests.rs", out_dir))?;
+    write_test(&mut rust_tests_file, &mut result.join(""))?;
+    Ok(())
+}
+
+fn get_run_test() -> String {
+    "fn run_test(directory: &str) -> (String, String) {
+    let i = format!(\"./tests/jsons/{}/input.json\", directory);
+    let o = format!(\"./tests/jsons/{}/output.json\", directory);
+    let input_path = Path::new(&i[..]);
+    let output_path = Path::new(&o[..]);
+    let input: Input = common::get_input_from_json(input_path).unwrap();
+    let desired_output: String = common::get_output_string_from_json(output_path).unwrap();
+    let output: FinalOutput = scheduler::run_scheduler(input);
+    (
+        serde_json::to_string_pretty(&output).unwrap(),
+        desired_output,
+    )
+}"
+    .to_string()
+}
--- tests/rust_tests.rs
+++ tests/rust_tests.rs
@@ -3,14 +3,7 @@ mod common;
 #[cfg(test)]
 use pretty_assertions::assert_eq;
 use scheduler::{FinalOutput, Input};
-use std::path::{Path, PathBuf};
-
-fn get_test_dirs() -> Result<Vec<PathBuf>, std::io::Error> {
-    let dirs = std::fs::read_dir("./tests/jsons")?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, std::io::Error>>()?;
-    Ok(dirs)
-}
+use std::path::Path;
 
 fn run_test(directory: &str) -> (String, String) {
     let i = format!("./tests/jsons/{}/input.json", directory);
@@ -20,318 +13,274 @@ fn run_test(directory: &str) -> (String, String) {
     let input: Input = common::get_input_from_json(input_path).unwrap();
     let desired_output: String = common::get_output_string_from_json(output_path).unwrap();
     let output: FinalOutput = scheduler::run_scheduler(input);
-
     (
         serde_json::to_string_pretty(&output).unwrap(),
         desired_output,
     )
 }
 #[test]
-fn run_all_tests() {
-    for dir in get_test_dirs()
-        .expect("Unable to read tests directory")
-        .iter()
-    {
-        if dir.file_name().unwrap().eq("benchmark-tests") || (!dir.is_dir()) {
-            continue;
-        }
-        println!("Run test {:#?}", dir.file_name().unwrap());
-        let (actual_output, desired_output) = run_test(dir.file_name().unwrap().to_str().unwrap());
-
-        assert_eq!(actual_output, desired_output);
-    }
-}
-
-#[test]
-fn basic_test_works() {
-    let (actual_output, desired_output) = run_test("basic-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn repetition_daily_works() {
-    let (actual_output, desired_output) = run_test("repetition-daily-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn repetition_daily_bounds_works() {
-    let (actual_output, desired_output) = run_test("repetition-daily-bounds-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn non_repetitive_bounds_multipledays_works() {
-    let (actual_output, desired_output) = run_test("non-rep-bounds-multipledays-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn single_day_many_goals_works() {
-    let (actual_output, desired_output) = run_test("singleday-manygoals-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_wednesday_works() {
-    let (actual_output, desired_output) = run_test("every-wednesday-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn realistic_schedule_works() {
-    let (actual_output, desired_output) = run_test("realistic-schedule-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn sleep() {
-    let (actual_output, desired_output) = run_test("sleep-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn split_1_works() {
-    let (actual_output, desired_output) = run_test("split-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn split_2_works() {
-    let (actual_output, desired_output) = run_test("split-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn split_3_works() {
+fn split_3() {
     let (actual_output, desired_output) = run_test("split-3");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn split_4_works() {
-    let (actual_output, desired_output) = run_test("split-4");
+fn basic_1() {
+    let (actual_output, desired_output) = run_test("basic-1");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn repetition_weekends_1_works() {
-    let (actual_output, desired_output) = run_test("repetition-weekends-1");
+fn every_wednesday_1() {
+    let (actual_output, desired_output) = run_test("every-wednesday-1");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn realistic_weekend_repetition_1_works() {
-    let (actual_output, desired_output) = run_test("realistic-weekend-repetition-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_3_days_works() {
-    let (actual_output, desired_output) = run_test("every-3-days");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn weekly1_works() {
-    let (actual_output, desired_output) = run_test("weekly-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn weekly2_works() {
-    let (actual_output, desired_output) = run_test("weekly-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_2_days_works() {
-    let (actual_output, desired_output) = run_test("every-2-days");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_60_days_works() {
-    let (actual_output, desired_output) = run_test("every-60-days");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn after_12_works() {
-    let (actual_output, desired_output) = run_test("after-12");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn before_7_works() {
-    let (actual_output, desired_output) = run_test("before-7");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn ui_test_case_without_start_or_deadline_works() {
-    let (actual_output, desired_output) = run_test("ui-test-case-without-start-or-deadline");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn slots_reduced_by_other_tasks_works() {
-    let (actual_output, desired_output) = run_test("slot-reduced-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_week_1_works() {
-    let (actual_output, desired_output) = run_test("x-per-week-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_week_2_works() {
-    let (actual_output, desired_output) = run_test("x-per-week-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_week_3_works() {
-    let (actual_output, desired_output) = run_test("x-per-week-3");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_day_1_works() {
-    let (actual_output, desired_output) = run_test("x-per-day-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_day_2_works() {
-    let (actual_output, desired_output) = run_test("x-per-day-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_day_3_works() {
-    let (actual_output, desired_output) = run_test("x-per-day-3");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn x_per_day_4_works() {
-    let (actual_output, desired_output) = run_test("x-per-day-4");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_2_hours_works() {
-    let (actual_output, desired_output) = run_test("every-2-hours");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_8_hours_works() {
-    let (actual_output, desired_output) = run_test("every-8-hours");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_2_hours_overlap_works() {
-    let (actual_output, desired_output) = run_test("every-2-hours-overlap");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_6_hours_works() {
-    let (actual_output, desired_output) = run_test("every-6-hours");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn impossible_1_works() {
-    let (actual_output, desired_output) = run_test("impossible-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn impossible_2_works() {
-    let (actual_output, desired_output) = run_test("impossible-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn non_midnight_start_deadline_works() {
-    let (actual_output, desired_output) = run_test("non-midnight-start-deadline");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn every_6_hours_2_works() {
-    let (actual_output, desired_output) = run_test("every-6-hours-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn flex_repeat_1_works() {
-    let (actual_output, desired_output) = run_test("flex-repeat-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn flex_repeat_2_works() {
-    let (actual_output, desired_output) = run_test("flex-repeat-2");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn flex_repeat_3_works() {
-    let (actual_output, desired_output) = run_test("flex-repeat-3");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn flex_duration_1_works() {
-    let (actual_output, desired_output) = run_test("flex-duration-1");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn flex_duration_2_works() {
+fn flex_duration_2() {
     let (actual_output, desired_output) = run_test("flex-duration-2");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn bug_215_works() {
-    let (actual_output, desired_output) = run_test("bug-215");
+fn every_21_days() {
+    let (actual_output, desired_output) = run_test("every-21-days");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn options_1_works() {
-    let (actual_output, desired_output) = run_test("options-1");
+fn singleday_manygoals_1() {
+    let (actual_output, desired_output) = run_test("singleday-manygoals-1");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn goals_dependency_works() {
-    let (actual_output, desired_output) = run_test("goals-dependency");
-    assert_eq!(actual_output, desired_output);
-}
-
-#[test]
-fn goals_dependency_2_works() {
+fn goals_dependency_2() {
     let (actual_output, desired_output) = run_test("goals-dependency-2");
     assert_eq!(actual_output, desired_output);
 }
-
 #[test]
-fn bug_236() {
-    let (actual_output, desired_output) = run_test("bug-236");
+fn split_1() {
+    let (actual_output, desired_output) = run_test("split-1");
     assert_eq!(actual_output, desired_output);
 }
-
+#[test]
+fn after_12() {
+    let (actual_output, desired_output) = run_test("after-12");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_week_3() {
+    let (actual_output, desired_output) = run_test("x-per-week-3");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_6_hours_2() {
+    let (actual_output, desired_output) = run_test("every-6-hours-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn weekdays_1() {
+    let (actual_output, desired_output) = run_test("weekdays-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_2_hours_overlap() {
+    let (actual_output, desired_output) = run_test("every-2-hours-overlap");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_3_days() {
+    let (actual_output, desired_output) = run_test("every-3-days");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_day_1() {
+    let (actual_output, desired_output) = run_test("x-per-day-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn options_1() {
+    let (actual_output, desired_output) = run_test("options-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_8_hours() {
+    let (actual_output, desired_output) = run_test("every-8-hours");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_60_days() {
+    let (actual_output, desired_output) = run_test("every-60-days");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn non_rep_bounds_multipledays_1() {
+    let (actual_output, desired_output) = run_test("non-rep-bounds-multipledays-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn slot_reduced_1() {
+    let (actual_output, desired_output) = run_test("slot-reduced-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_2_days() {
+    let (actual_output, desired_output) = run_test("every-2-days");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn before_7() {
+    let (actual_output, desired_output) = run_test("before-7");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn flex_duration_1() {
+    let (actual_output, desired_output) = run_test("flex-duration-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_week_2() {
+    let (actual_output, desired_output) = run_test("x-per-week-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn flex_repeat_2() {
+    let (actual_output, desired_output) = run_test("flex-repeat-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_day_4() {
+    let (actual_output, desired_output) = run_test("x-per-day-4");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_6_hours() {
+    let (actual_output, desired_output) = run_test("every-6-hours");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn sleep_1() {
+    let (actual_output, desired_output) = run_test("sleep-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn split_2() {
+    let (actual_output, desired_output) = run_test("split-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn flex_repeat_1() {
+    let (actual_output, desired_output) = run_test("flex-repeat-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn every_2_hours() {
+    let (actual_output, desired_output) = run_test("every-2-hours");
+    assert_eq!(actual_output, desired_output);
+}
 #[test]
 fn planned_goals_hierarchy() {
     let (actual_output, desired_output) = run_test("planned-goals-hierarchy");
     assert_eq!(actual_output, desired_output);
 }
+#[test]
+fn repetition_weekends_1() {
+    let (actual_output, desired_output) = run_test("repetition-weekends-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn goals_hierarchy() {
+    let (actual_output, desired_output) = run_test("goals-hierarchy");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn split_4() {
+    let (actual_output, desired_output) = run_test("split-4");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn bug_215() {
+    let (actual_output, desired_output) = run_test("bug-215");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn basic_2() {
+    let (actual_output, desired_output) = run_test("basic-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn realistic_schedule_1() {
+    let (actual_output, desired_output) = run_test("realistic-schedule-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn realistic_weekend_repetition_1() {
+    let (actual_output, desired_output) = run_test("realistic-weekend-repetition-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_week_1() {
+    let (actual_output, desired_output) = run_test("x-per-week-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn impossible_1() {
+    let (actual_output, desired_output) = run_test("impossible-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn flex_repeat_3() {
+    let (actual_output, desired_output) = run_test("flex-repeat-3");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn goals_dependency() {
+    let (actual_output, desired_output) = run_test("goals-dependency");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn ui_test_case_without_start_or_deadline() {
+    let (actual_output, desired_output) = run_test("ui-test-case-without-start-or-deadline");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn impossible_2() {
+    let (actual_output, desired_output) = run_test("impossible-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_day_3() {
+    let (actual_output, desired_output) = run_test("x-per-day-3");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn weekly_1() {
+    let (actual_output, desired_output) = run_test("weekly-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn x_per_day_2() {
+    let (actual_output, desired_output) = run_test("x-per-day-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn weekly_2() {
+    let (actual_output, desired_output) = run_test("weekly-2");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn bug_236() {
+    let (actual_output, desired_output) = run_test("bug-236");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn repetition_daily_bounds_1() {
+    let (actual_output, desired_output) = run_test("repetition-daily-bounds-1");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn non_midnight_start_deadline() {
+    let (actual_output, desired_output) = run_test("non-midnight-start-deadline");
+    assert_eq!(actual_output, desired_output);
+}
+#[test]
+fn repetition_daily_1() {
+    let (actual_output, desired_output) = run_test("repetition-daily-1");
+    assert_eq!(actual_output, desired_output);
+} 
+
