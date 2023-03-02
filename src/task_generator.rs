c50e0c2ad88063b2528ca8320ba4cca22374d54f
mustafa-eldeeb <darshm466@gmail.com>
2023-03-02 21:12:02 +0200

    fix borrowing errors

--- src/task_generator.rs
+++ src/task_generator.rs
@@ -13,8 +13,8 @@ pub fn task_generator(
 ) -> GeneratedTasks {
     let mut counter: usize = 0;
     let mut tasks = vec![];
-    let goals = add_filler(goals);
-    let goals = add_repeat(goals);
+    let mut goals = add_filler(goals);
+    add_repeat(& mut goals);
 
     for goal in goals {
         let goals_vec = goal.generate_tasks(calendar_start, calendar_end, &mut counter);
@@ -27,8 +27,8 @@ pub fn task_generator(
     }
 }
 
-fn add_repeat(goals: Vec<Goal>) -> Vec<Goal> {
-    for goal in goals.iter_mut() {
+fn add_repeat( goals: &mut Vec<Goal>){
+    for goal in goals.clone().iter_mut(){
         if let Some(Repetition::FlexWeekly(min, max)) = goal.repeat {
             //Flex repeat goals are handled as follows:
             //If given a goal with 3-5/week, create two goals: One with 3/week and another
@@ -38,7 +38,7 @@ fn add_repeat(goals: Vec<Goal>) -> Vec<Goal> {
             goal.repeat = Some(Repetition::Weekly(min));
             goal2.repeat = Some(Repetition::Weekly(max - min));
             goal2.tags.push(Tag::Optional);
-            goals.push(goal2);
+            goals.push(goal2.to_owned());
         } else if goal.duration.1.is_some() {
             //if this is a flex duration e.g. '35-40h weekly', create two goals: One with 35/week
             //and another with 5/week. The 5/week goal should be tagged optional. Then turn each
@@ -54,11 +54,11 @@ fn add_repeat(goals: Vec<Goal>) -> Vec<Goal> {
             let mut goals: Vec<Goal> = vec![];
             let g = get_1_hr_goals(goal.to_owned());
             goals.extend(g.into_iter());
-            let g = get_1_hr_goals(goal2.to_owned());
+            let g = get_1_hr_goals(goal2);
             goals.extend(g.into_iter());
         }
     }
-    goals
+    //goals
 }
 
 fn get_1_hr_goals(goal: Goal) -> Vec<Goal> {
