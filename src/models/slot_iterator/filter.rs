1df0b4299aac1d5be2cf24b7c68fc5808fd71578
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-04-17 07:43:04 +0200

    chore: removed unneeded param

--- src/models/slot_iterator/filter.rs
+++ src/models/slot_iterator/filter.rs
@@ -9,11 +9,11 @@ impl TimeSlotsIterator {
             Some(time_filter) => {
                 let _time_filter_str: String = time_filter.to_string();
 
-                if let Some(slots) = apply_timing_filter(self, time_filter) {
+                if let Some(slots) = apply_timing_filter(self) {
                     self.timeline = slots
                 }
 
-                if let Some(slots) = apply_on_days_filter(self, time_filter) {
+                if let Some(slots) = apply_on_days_filter(self) {
                     self.timeline = slots
                 }
             }
@@ -22,7 +22,7 @@ impl TimeSlotsIterator {
     }
 }
 
-fn apply_on_days_filter(
+fn _apply_not_on_filter(
     slot_iterator: &TimeSlotsIterator,
     time_filter: &TimeFilter,
 ) -> Option<Vec<Slot>> {
@@ -48,87 +48,117 @@ fn apply_on_days_filter(
     }
 }
 
-fn apply_timing_filter(
-    slot_iterator: &TimeSlotsIterator,
-    time_filter: &TimeFilter,
-) -> Option<Vec<Slot>> {
-    if time_filter.after_time.is_some() || time_filter.before_time.is_some() {
-        let mut result: Vec<Slot> = vec![];
-        for slot in slot_iterator.timeline.iter() {
-            let _slot_str = slot.to_string();
-            let mut daily_slots = slot.divide_in_days();
-            if time_filter.after_time.is_some() && time_filter.before_time.is_some() {
-                //after and before time
-                for daily_slot in daily_slots.iter() {
-                    let _daily_slot_str = daily_slot.to_string();
+fn apply_on_days_filter(slot_iterator: &TimeSlotsIterator) -> Option<Vec<Slot>> {
+    if let Some(time_filter) = &slot_iterator.filters {
+        let _time_filter_str: String = time_filter.to_string();
+        match &time_filter.on_days {
+            Some(on_days) => {
+                let mut result: Vec<Slot> = vec![];
+                for slot in slot_iterator.timeline.iter() {
+                    let daily_slots = slot.divide_in_days();
 
-                    let before_datetime = daily_slot
-                        .start
-                        .with_hour(time_filter.before_time.unwrap() as u32)
-                        .unwrap();
-                    let after_datetime = daily_slot
-                        .start
-                        .with_hour(time_filter.after_time.unwrap() as u32)
-                        .unwrap();
-
-                    let _after_datetime_str = after_datetime.to_string();
-                    let _before_datetime_str = before_datetime.to_string();
-
-                    if after_datetime > before_datetime {
-                        if daily_slot.start < before_datetime {
-                            result.push({
-                                Slot {
-                                    start: daily_slot.start,
-                                    end: before_datetime,
-                                }
-                            });
+                    for daily_slot in daily_slots.iter() {
+                        // Check if the weekday matches with the given on days filter value
+                        //  and push it to result vector if true.
+                        let start_day: String = slot.start.weekday().to_string();
+                        if on_days.contains(&Day::from(start_day)) {
+                            result.push(*daily_slot);
                         }
-                        if daily_slot.end > after_datetime {
+                    }
+                }
+
+                Some(result)
+            }
+            None => None,
+        }
+    } else {
+        None
+    }
+}
+
+fn apply_timing_filter(slot_iterator: &TimeSlotsIterator) -> Option<Vec<Slot>> {
+    if let Some(time_filter) = &slot_iterator.filters {
+        let _time_filter_str: String = time_filter.to_string();
+        if time_filter.after_time.is_some() || time_filter.before_time.is_some() {
+            let mut result: Vec<Slot> = vec![];
+            for slot in slot_iterator.timeline.iter() {
+                let _slot_str = slot.to_string();
+                let mut daily_slots = slot.divide_in_days();
+                if time_filter.after_time.is_some() && time_filter.before_time.is_some() {
+                    //after and before time
+                    for daily_slot in daily_slots.iter() {
+                        let _daily_slot_str = daily_slot.to_string();
+
+                        let before_datetime = daily_slot
+                            .start
+                            .with_hour(time_filter.before_time.unwrap() as u32)
+                            .unwrap();
+                        let after_datetime = daily_slot
+                            .start
+                            .with_hour(time_filter.after_time.unwrap() as u32)
+                            .unwrap();
+
+                        let _after_datetime_str = after_datetime.to_string();
+                        let _before_datetime_str = before_datetime.to_string();
+
+                        if after_datetime > before_datetime {
+                            if daily_slot.start < before_datetime {
+                                result.push({
+                                    Slot {
+                                        start: daily_slot.start,
+                                        end: before_datetime,
+                                    }
+                                });
+                            }
+                            if daily_slot.end > after_datetime {
+                                result.push({
+                                    Slot {
+                                        start: after_datetime,
+                                        end: daily_slot.end,
+                                    }
+                                });
+                            }
+                        } else {
                             result.push({
                                 Slot {
                                     start: after_datetime,
-                                    end: daily_slot.end,
+                                    end: before_datetime,
                                 }
-                            });
+                            })
                         }
-                    } else {
-                        result.push({
-                            Slot {
-                                start: after_datetime,
-                                end: before_datetime,
-                            }
-                        })
+                    }
+                }
+                if time_filter.after_time.is_some() && time_filter.before_time.is_none() {
+                    //only after time
+                    for daily_slot in daily_slots.iter_mut() {
+                        let after_datetime = daily_slot
+                            .start
+                            .with_hour(time_filter.after_time.unwrap() as u32)
+                            .unwrap();
+                        if daily_slot.start < after_datetime {
+                            daily_slot.start = after_datetime;
+                        }
+                        result.push(*daily_slot);
+                    }
+                }
+                if time_filter.after_time.is_none() && time_filter.before_time.is_some() {
+                    //only before time
+                    for daily_slot in daily_slots.iter_mut() {
+                        let before_datetime = daily_slot
+                            .start
+                            .with_hour(time_filter.before_time.unwrap() as u32)
+                            .unwrap();
+                        if daily_slot.end > before_datetime {
+                            daily_slot.end = before_datetime;
+                        }
+                        result.push(*daily_slot);
                     }
                 }
             }
-            if time_filter.after_time.is_some() && time_filter.before_time.is_none() {
-                //only after time
-                for daily_slot in daily_slots.iter_mut() {
-                    let after_datetime = daily_slot
-                        .start
-                        .with_hour(time_filter.after_time.unwrap() as u32)
-                        .unwrap();
-                    if daily_slot.start < after_datetime {
-                        daily_slot.start = after_datetime;
-                    }
-                    result.push(*daily_slot);
-                }
-            }
-            if time_filter.after_time.is_none() && time_filter.before_time.is_some() {
-                //only before time
-                for daily_slot in daily_slots.iter_mut() {
-                    let before_datetime = daily_slot
-                        .start
-                        .with_hour(time_filter.before_time.unwrap() as u32)
-                        .unwrap();
-                    if daily_slot.end > before_datetime {
-                        daily_slot.end = before_datetime;
-                    }
-                    result.push(*daily_slot);
-                }
-            }
+            return Some(result);
         }
-        return Some(result);
+        None
+    } else {
+        None
     }
-    None
 }
