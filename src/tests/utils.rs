6195ad98e9617c2073849c8594078875300d37de
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-04-25 09:43:44 +0200

    wip: developed test util function get_timeline

--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -1,4 +1,4 @@
-use crate::models::slot::Slot;
+use crate::models::{slot::Slot, timeline::Timeline};
 use chrono::{Duration, NaiveDate};
 
 /// Get a slot based on Calendar datetime with Duration
@@ -17,4 +17,11 @@ pub fn get_slot(
     let end = start + duration;
 
     Slot { start, end }
+}
+
+pub fn get_timeline(duration: Duration, year: i32, month: u32, day: u32) -> Timeline {
+    let slot = get_slot(duration, year, month, day, 0, 0);
+    Timeline {
+        slots: vec![slot].into_iter().collect(),
+    }
 }
