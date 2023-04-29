0ee5ba58f555fcf934dd0b424998ca5a2f1d44b2
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-04-29 12:16:25 +0300

    test: added test case based on issue 276 to check output from old and new apply_filter function

--- src/tests/filter/cmp_old_filter.rs
+++ src/tests/filter/cmp_old_filter.rs
@@ -142,3 +142,61 @@ fn test_i284_7days() {
     // assert!(false);
     assert_eq!(timeline_by_new_filter, timeline_by_old_filter);
 }
+
+#[test]
+fn test_i276() {
+    // Simulating issue-276-weekdays-filter-on-budget
+    let init_year = 2023;
+    let init_month: u32 = 03;
+    let init_day: u32 = 9;
+    let end_day: u32 = 20;
+
+    let init_duration = Duration::days((end_day - init_day) as i64);
+    let start_time: u32 = 8;
+    let end_time: u32 = 16;
+
+    let after_time: Option<usize> = Some(start_time as usize);
+    let before_time: Option<usize> = Some(end_time as usize);
+    let on_days: Option<Vec<Day>> = Some(vec![Day::Mon, Day::Tue, Day::Wed, Day::Thu, Day::Fri]);
+    let not_on: Option<Vec<Slot>> = None;
+
+    let start = NaiveDate::from_ymd_opt(init_year, init_month, init_day)
+        .unwrap()
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+    let deadline = NaiveDate::from_ymd_opt(init_year, init_month, end_day)
+        .unwrap()
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+
+    let filters = Some(TimeFilter {
+        before_time,
+        after_time,
+        on_days,
+        not_on,
+    });
+    dbg!(&start, &deadline, &filters);
+
+    let timeline = get_timeline_single_slot(init_duration, init_year, init_month, init_day);
+    dbg!(&timeline);
+
+    let mut slot_iterator = TimeSlotsIterator {
+        timeline: timeline.clone(),
+        repetition: None,
+        filters: filters.clone(),
+        current_start_position: start,
+    };
+
+    // Test on the old filter function
+    slot_iterator.apply_filters();
+    let timeline_by_old_filter = slot_iterator.timeline;
+
+    // Test on the new filter function
+    let timeline_by_new_filter = apply_filter(&timeline, &filters);
+
+    dbg!(&timeline_by_old_filter);
+    dbg!(&timeline_by_new_filter);
+
+    // assert!(false);
+    assert_eq!(timeline_by_new_filter, timeline_by_old_filter);
+}
