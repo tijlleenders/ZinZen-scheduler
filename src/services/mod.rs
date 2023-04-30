8312d62520b125c0c31306dec932620f1223471c
Tijl Leenders <tijl.leenders@gmail.com>
2023-04-30 10:16:04 +0200

    add high level services descriptions

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,3 +1,6 @@
+/// Merges all FinalSlots Timelines from all Tasks into a single Timeline, formatted for the frontend
 pub mod output;
+/// Generates Tasks from Goals, giving each Task their [MECE part](https://en.wikipedia.org/wiki/MECE_principle) of the Goal MaybeSlots Timeline
 pub mod task_generator;
+/// For each Task, reduce the MaybeSlots Timeline into a FinalSlots Timeline, where the next Task to pick is calculated anew after every handled Task
 pub mod task_placer;
