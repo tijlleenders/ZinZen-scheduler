7e778a215350eae43b8cb0f29978aa0d2a3ac0b3
Tijl Leenders <tijl.leenders@gmail.com>
2023-05-01 18:53:44 +0200

    Input doc

--- src/models/input.rs
+++ src/models/input.rs
@@ -5,11 +5,11 @@ use chrono::prelude::*;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 
-#[derive(Deserialize, Debug)]
-/// The front end passes data into the scheduler via an Input object.
-/// It defines the calendar start and end, and a collection of goals that the scheduler needs to schedule.
-/// On the scheduler side, the Input is passed as a ['JSValue'](https://rustwasm.github.io/wasm-bindgen/api/wasm_bindgen/struct.JsValue.html).
+/// The front end gets a Calendar by passing a JSON data into the scheduler, via an Input object.
+/// It has the requested calendar start and end, and a collection of goals that the scheduler needs to schedule.
+/// On the scheduler side, the JSON is received as a ['JSValue'](https://rustwasm.github.io/wasm-bindgen/api/wasm_bindgen/struct.JsValue.html).
 /// This allows it to be deserialized into this struct.
+#[derive(Deserialize, Debug)]
 pub struct Input {
     #[serde(rename = "startDate")]
     pub calendar_start: NaiveDateTime,
