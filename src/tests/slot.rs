2e0c3f30e1442715fb7b466193f2a9f649d50ee9
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-05-03 00:07:44 +0300

    wip: test cases for slot functions

--- src/tests/slot.rs
+++ src/tests/slot.rs
@@ -110,3 +110,119 @@ fn test_subtract_when_no_overlap() {
 
     assert_eq!(expected_result, result);
 }
+
+#[test]
+fn test_is_conflicts_with() {
+    let year = 2023;
+    let month = 5;
+    let day = 5;
+    let hour: u32 = 0;
+    let min: u32 = 0;
+    let duration = Duration::hours(10);
+
+    let base_slot = get_slot(duration, year, month, day, hour, min);
+    let conflicted_last_of_base = get_slot(duration, year, month, day, 09, min);
+    let conflicted_start_of_base = get_slot(duration, year, month, day - 1, 20, min);
+    let not_conflicted_with_base = get_slot(duration, year, month, day + 1, hour, min);
+    dbg!(
+        &base_slot,
+        &conflicted_last_of_base,
+        &conflicted_start_of_base,
+        &not_conflicted_with_base
+    );
+
+    let is_conflicted_start_of_base = base_slot.is_conflicts_with(&conflicted_start_of_base);
+    dbg!(&is_conflicted_start_of_base);
+
+    let is_conflicted_last_of_base = base_slot.is_conflicts_with(&conflicted_last_of_base);
+    dbg!(&is_conflicted_last_of_base);
+
+    let is_not_conflicted_with_base = base_slot.is_conflicts_with(&not_conflicted_with_base);
+    dbg!(&is_not_conflicted_with_base);
+
+    // assert_eq!(expected_result, result);
+    assert_eq!(true, is_conflicted_last_of_base);
+    assert_eq!(true, is_conflicted_start_of_base);
+    assert_eq!(false, is_not_conflicted_with_base);
+}
+
+#[test]
+fn test_is_contains_slot() {
+    let year = 2023;
+    let month = 5;
+    let day = 5;
+    let hour: u32 = 0;
+    let min: u32 = 0;
+    let duration = Duration::hours(10);
+
+    let base_slot = get_slot(duration, year, month, day, hour, min);
+    let contained_in_base = get_slot(Duration::hours(3), year, month, day, 02, min);
+    let equal_to_base = get_slot(duration, year, month, day, hour, min);
+    let overflow_base_from_start = get_slot(Duration::hours(3), year, month, day - 1, 23, min);
+    let overflow_base_from_end = get_slot(Duration::hours(3), year, month, day, 09, min);
+    let not_contained_in_base = get_slot(duration, year, month, day + 1, hour, min);
+    dbg!(
+        &base_slot,
+        &contained_in_base,
+        &equal_to_base,
+        &overflow_base_from_start,
+        &overflow_base_from_end,
+        &not_contained_in_base
+    );
+
+    let is_contained_in_base = base_slot.is_contains_slot(&contained_in_base);
+    dbg!(&is_contained_in_base);
+
+    let is_equal_to_base_contained = base_slot.is_contains_slot(&equal_to_base);
+    dbg!(&is_equal_to_base_contained);
+
+    let is_overflow_base_from_start = base_slot.is_contains_slot(&overflow_base_from_start);
+    dbg!(&is_overflow_base_from_start);
+
+    let is_overflow_base_from_end = base_slot.is_contains_slot(&overflow_base_from_end);
+    dbg!(&is_overflow_base_from_end);
+
+    let is_not_contained_in_base = base_slot.is_contains_slot(&not_contained_in_base);
+    dbg!(&is_not_contained_in_base);
+
+    assert_eq!(true, is_contained_in_base);
+    assert_eq!(true, is_equal_to_base_contained);
+    assert_eq!(false, is_overflow_base_from_start);
+    assert_eq!(false, is_overflow_base_from_end);
+    assert_eq!(false, is_not_contained_in_base);
+}
+
+#[test]
+fn test_is_intersect_with_slot() {
+    let year = 2023;
+    let month = 5;
+    let day = 5;
+    let hour: u32 = 0;
+    let min: u32 = 0;
+    let duration = Duration::hours(10);
+
+    let base_slot = get_slot(duration, year, month, day, hour, min);
+    let intersected_last_of_base = get_slot(duration, year, month, day, 09, min);
+    let intersected_start_of_base = get_slot(duration, year, month, day - 1, 20, min);
+    let not_intersected_with_base = get_slot(duration, year, month, day + 1, hour, min);
+    dbg!(
+        &base_slot,
+        &intersected_last_of_base,
+        &intersected_start_of_base,
+        &not_intersected_with_base
+    );
+
+    let is_intersected_start_of_base = base_slot.is_intersect_with_slot(&intersected_start_of_base);
+    dbg!(&is_intersected_start_of_base);
+
+    let is_intersected_last_of_base = base_slot.is_intersect_with_slot(&intersected_last_of_base);
+    dbg!(&is_intersected_last_of_base);
+
+    let is_not_intersected_with_base = base_slot.is_intersect_with_slot(&not_intersected_with_base);
+    dbg!(&is_not_intersected_with_base);
+
+    // assert_eq!(expected_result, result);
+    assert_eq!(true, is_intersected_last_of_base);
+    assert_eq!(true, is_intersected_start_of_base);
+    assert_eq!(false, is_not_intersected_with_base);
+}
