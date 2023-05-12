a3ed141b4273f49263ee778605553754f8df8251
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-05-12 03:38:28 +0300

    fix: SlotIterator can return slots based on custom interval. test: 2 test cases developed

--- src/models/slot/iterator.rs
+++ src/models/slot/iterator.rs
@@ -39,42 +39,114 @@ impl SlotIterator {
 }
 
 impl Iterator for SlotIterator {
-    type Item = NaiveDateTime;
+    type Item = Slot;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.pointer >= self.slot.end {
             return None;
         }
-        self.pointer += self.interval;
-        Some(self.pointer)
+        let next_pointer = self.pointer + self.interval;
+
+        let slot = Slot {
+            start: self.pointer,
+            end: next_pointer,
+        };
+        self.pointer = next_pointer;
+
+        Some(slot)
     }
 }
 
 //test case for Slot Iterator impl
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
     use chrono::NaiveDateTime;
 
+    /// Testing walk through a slot from 5 days and iterate over each day
     #[test]
-    fn test_count_interval_by_iterator_for_4_days() {
-        let expected_count: usize = 4;
+    fn test_walk_through_by_day() {
+        let slot_duration = Duration::days(5);
+        let interval_duration = Duration::days(1);
 
-        let slot = Slot {
-            start: NaiveDateTime::parse_from_str("2023-04-26T00:00:00", "%Y-%m-%dT%H:%M:%S")
-                .unwrap(),
-            end: NaiveDateTime::parse_from_str("2023-04-30T00:00:00", "%Y-%m-%dT%H:%M:%S").unwrap(),
-        };
-        let slot_iterator = SlotIterator::initialize(slot);
+        let slot = Slot::mock(slot_duration, 2023, 05, 1, 0, 0);
+        dbg!(&slot);
+
+        let expected_result: Vec<Slot> = vec![
+            Slot::mock(Duration::days(1), 2023, 05, 1, 0, 0),
+            Slot::mock(Duration::days(1), 2023, 05, 2, 0, 0),
+            Slot::mock(Duration::days(1), 2023, 05, 3, 0, 0),
+            Slot::mock(Duration::days(1), 2023, 05, 4, 0, 0),
+            Slot::mock(Duration::days(1), 2023, 05, 5, 0, 0),
+        ];
+        dbg!(&expected_result);
+
+        let slot_iterator = SlotIterator::new(slot, interval_duration);
         dbg!(&slot, &slot_iterator);
 
-        let mut results: Vec<NaiveDateTime> = vec![];
-        for pointer in slot_iterator {
-            dbg!(pointer);
-            results.push(pointer)
-        }
+        let mut result: Vec<Slot> = vec![];
 
-        assert_eq!(expected_count, results.len());
+        for slot in slot_iterator {
+            result.push(slot);
+        }
+        dbg!(&expected_result, &result);
+
+        assert_eq!(expected_result, result);
+    }
+
+    /// Testing walk through a slot from 5 days and iterate over each day
+    #[test]
+    fn test_walk_through_by_hours() {
+        let slot_duration = Duration::hours(5);
+        let interval_duration = Duration::hours(1);
+
+        let slot = Slot::mock(slot_duration, 2023, 05, 1, 0, 0);
+        dbg!(&slot);
+
+        let expected_result: Vec<Slot> = vec![
+            Slot::mock(Duration::hours(1), 2023, 05, 1, 0, 0),
+            Slot::mock(Duration::hours(1), 2023, 05, 1, 1, 0),
+            Slot::mock(Duration::hours(1), 2023, 05, 1, 2, 0),
+            Slot::mock(Duration::hours(1), 2023, 05, 1, 3, 0),
+            Slot::mock(Duration::hours(1), 2023, 05, 1, 4, 0),
+        ];
+        dbg!(&expected_result);
+
+        let slot_iterator = SlotIterator::new(slot, interval_duration);
+        dbg!(&slot, &slot_iterator);
+
+        let mut result: Vec<Slot> = vec![];
+
+        for slot in slot_iterator {
+            result.push(slot);
+        }
+        dbg!(&expected_result, &result);
+
+        assert_eq!(expected_result, result);
+    }
+
+    #[test]
+    fn test_count_interval_by_iterator_for_4_days() {
+        todo!("test not implemented");
+        // let expected_count: usize = 4;
+
+        // let slot = Slot {
+        //     start: NaiveDateTime::parse_from_str("2023-04-26T00:00:00", "%Y-%m-%dT%H:%M:%S")
+        //         .unwrap(),
+        //     end: NaiveDateTime::parse_from_str("2023-04-30T00:00:00", "%Y-%m-%dT%H:%M:%S").unwrap(),
+        // };
+        // let slot_iterator = SlotIterator::initialize(slot);
+        // dbg!(&slot, &slot_iterator);
+
+        // let mut results: Vec<NaiveDateTime> = vec![];
+        // for pointer in slot_iterator {
+        //     dbg!(pointer);
+        //     results.push(pointer)
+        // }
+
+        // assert_eq!(expected_count, results.len());
     }
 
     #[test]
