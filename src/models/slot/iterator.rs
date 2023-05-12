cae5c5c3fc4be3c950a89dfd82a1ed93fa607f32
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-05-12 04:07:43 +0300

    ref: removd interval_count func with test which not need at the moment

--- src/models/slot/iterator.rs
+++ src/models/slot/iterator.rs
@@ -1,7 +1,8 @@
 use super::Slot;
 use chrono::{Duration, NaiveDateTime};
 
-/// Iterator for a `Slot` and provide some functionalities like count per duration interval
+/// Iterator for a `Slot` and provide functionalities to walk through
+/// a `Slot` based on custom interval duration
 #[derive(Debug, Clone)]
 pub struct SlotIterator {
     slot: Slot,
@@ -26,15 +27,6 @@ impl SlotIterator {
             pointer: slot.start,
             interval: interval_duration,
         }
-    }
-
-    /// Get count between slot.start and slot.end based on Duration interval
-    /// Example: if duration interval is 1 min, so it will count minutes
-    ///  between slot.start and slot.end
-    pub fn interval_count(&self) -> usize {
-        let mut count = 0;
-        self.clone().for_each(|_| count += 1);
-        count
     }
 }
 
@@ -63,7 +55,6 @@ mod tests {
     use std::vec;
 
     use super::*;
-    use chrono::NaiveDateTime;
 
     /// Testing walk through a slot from 5 days and iterate over each day
     #[test]
@@ -125,61 +116,5 @@ mod tests {
         dbg!(&expected_result, &result);
 
         assert_eq!(expected_result, result);
-    }
-
-    #[test]
-    fn test_interval_count_as_interval_duration_1_day() {
-        let interval_duration = Duration::days(1);
-        let expected_count: usize = 4;
-
-        let slot = Slot {
-            start: NaiveDateTime::parse_from_str("2023-04-26T00:00:00", "%Y-%m-%dT%H:%M:%S")
-                .unwrap(),
-            end: NaiveDateTime::parse_from_str("2023-04-30T00:00:00", "%Y-%m-%dT%H:%M:%S").unwrap(),
-        };
-        let slot_iterator = SlotIterator::new(slot, interval_duration);
-        dbg!(&slot, &slot_iterator);
-
-        let result = slot_iterator.interval_count();
-        dbg!(&result);
-        assert_eq!(expected_count, result);
-    }
-
-    #[test]
-    fn test_interval_count_as_interval_duration_1_mintue() {
-        let interval_duration = Duration::minutes(1);
-        let expected_count: usize = 5760;
-
-        let slot = Slot {
-            start: NaiveDateTime::parse_from_str("2023-04-26T00:00:00", "%Y-%m-%dT%H:%M:%S")
-                .unwrap(),
-            end: NaiveDateTime::parse_from_str("2023-04-30T00:00:00", "%Y-%m-%dT%H:%M:%S").unwrap(),
-        };
-        let slot_iterator = SlotIterator::new(slot, interval_duration);
-        dbg!(&slot, &slot_iterator);
-
-        let result = slot_iterator.interval_count();
-        dbg!(&result);
-
-        assert_eq!(expected_count, result);
-    }
-
-    #[test]
-    fn test_interval_count_as_interval_duration_1_hour() {
-        let interval_duration = Duration::hours(1);
-        let expected_count: usize = 96;
-
-        let slot = Slot {
-            start: NaiveDateTime::parse_from_str("2023-04-26T00:00:00", "%Y-%m-%dT%H:%M:%S")
-                .unwrap(),
-            end: NaiveDateTime::parse_from_str("2023-04-30T00:00:00", "%Y-%m-%dT%H:%M:%S").unwrap(),
-        };
-        let slot_iterator = SlotIterator::new(slot, interval_duration);
-        dbg!(&slot, &slot_iterator);
-
-        let result = slot_iterator.interval_count();
-        dbg!(&result);
-
-        assert_eq!(expected_count, result);
     }
 }
