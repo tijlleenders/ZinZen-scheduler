d1c81f7fae7d298700e1ab84967ec28e90f675c6
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-05-30 20:48:09 +0300

    wip: more tests added for flexibility

--- src/services/flexibility.rs
+++ src/services/flexibility.rs
@@ -3,32 +3,49 @@ use crate::models::task::{Task, TaskStatus};
 impl Task {
     /// Calculate flexibility of a task slots
     pub fn calculate_flexibility(&mut self) {
-        dbg!(&self);
-        // Todo 2023-05-29  | add rules for other TaskStatus options
         if self.status == TaskStatus::Scheduled || self.status == TaskStatus::Impossible {
             return;
         }
 
-        let mut flexibility = 0;
         let task_duration = self.duration;
-
-        for slot in &self.slots {
+        let flexibility = self.slots.iter().fold(0, |acc, slot| {
             let slot_duration = slot.duration_as_hours();
-            dbg!(&slot);
             if slot_duration < task_duration {
-                flexibility += slot_duration;
-                continue;
+                acc + slot_duration
+            } else {
+                acc + slot_duration - task_duration + 1
             }
-            //todo check correctness
-            flexibility += slot_duration - task_duration + 1;
-        }
-        self.flexibility = flexibility;
+        });
 
-        if self.flexibility == 0 {
-            self.status = TaskStatus::Impossible
+        if flexibility == 0 {
+            self.status = TaskStatus::Impossible;
         }
-        dbg!(&self);
+
+        self.flexibility = flexibility;
     }
+}
+
+fn calc_flex(task: &Task) -> usize {
+    let first_slot_time = task.slots[0].start;
+    let start_time = first_slot_time;
+    let duration = task.duration;
+
+    let mut min_difference = start_time
+        .signed_duration_since(first_slot_time)
+        .num_hours();
+
+    for slot in task.slots.iter() {
+        dbg!(&slot);
+        let difference = slot.start.signed_duration_since(start_time).num_hours();
+
+        if difference < min_difference {
+            min_difference = difference;
+        }
+    }
+
+    let flex = min_difference as usize + duration;
+    dbg!(&flex);
+    flex
 }
 
 #[cfg(test)]
@@ -71,6 +88,7 @@ mod tests {
 
     /// Test when Task Status is Uninitialized
     #[test]
+    #[ignore]
     fn test_status_uninitialized() {
         // TODO 2023-05-29  |  add rules for TaskStatus Uninitialized
         todo!("test_status_Uninitialized");
@@ -78,6 +96,7 @@ mod tests {
 
     /// Test when Task Status is Blocked
     #[test]
+    #[ignore]
     fn test_status_blocked() {
         // TODO 2023-05-29  |  add rules for TaskStatus Blocked
         todo!("test_status_blocked");
@@ -85,45 +104,126 @@ mod tests {
 
     /// Test when Task Status is BudgetMinWaitingForAdjustment
     #[test]
+    #[ignore]
     fn test_status_budget_min_wait_adj() {
         // TODO 2023-05-29  |  add rules for TaskStatus BudgetMinWaitingForAdjustment
         todo!("test_status_budget_min_wait_adj");
     }
 
-    ///
+    /// Simulate one Task in test case bug_215 which is Sleep
+    /// ```
+    ///     Task: Sleep,
+    ///     Duration: 8 hours
+    ///     Timing: 22-08
+    /// ```
     #[test]
-    fn test_calculate_flexibility() {
-        // todo!("test_calculate_flexibility");
-        // // Create a task with two slots: one 2-hour slot and one 4-hour slot
-        // let mut task = Task::mock(status, slots)
-        // task.add_slot(Slot::new(
-        //     LocalTime::from_hms(9, 0, 0),
-        //     LocalTime::from_hms(11, 0, 0),
-        // ));
-        // task.add_slot(Slot::new(
-        //     LocalTime::from_hms(13, 0, 0),
-        //     LocalTime::from_hms(17, 0, 0),
-        // ));
+    fn test_sleep_alone() {
+        let mut task = Task::mock(
+            8,
+            0,
+            TaskStatus::ReadyToSchedule,
+            vec![
+                Slot::mock(Duration::hours(8), 2023, 01, 03, 0, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 03, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 04, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 05, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 06, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 07, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 08, 22, 0),
+                // Slot::mock(Duration::hours(2), 2023, 01, 09, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 09, 22, 0),
+            ],
+        );
+        dbg!(&task);
 
-        // // Calculate flexibility
-        // task.calculate_flexibility();
+        task.calculate_flexibility();
+        dbg!(&task);
 
-        // // Check that flexibility is correct
-        // assert_eq!(task.flexibility, 3); // 2 hours from first slot + 1 hour from second slot
+        assert_eq!(19, task.flexibility);
+    }
 
-        // // Check that status is still New
-        // assert_eq!(task.status, TaskStatus::New);
+    /// Simulate a Task in test case bug_215 which is
+    /// have highest flexibility because it can be assigned anytime
+    /// ```
+    ///     Task: Refreshing,
+    ///     Duration: 1 hour
+    ///     Timing: anytime
+    /// ```
+    #[test]
+    fn test_task_anytime_1hr() {
+        let mut task = Task::mock(
+            1,
+            0,
+            TaskStatus::ReadyToSchedule,
+            vec![Slot::mock(Duration::days(7), 2023, 01, 03, 0, 0)],
+        );
+        dbg!(&task);
 
-        // // Schedule the task by taking one of the slots
-        // task.schedule(LocalTime::from_hms(9, 0, 0));
+        task.calculate_flexibility();
+        dbg!(&task);
 
-        // // Calculate flexibility again (should not do anything since task is scheduled)
-        // task.calculate_flexibility();
+        assert_eq!(168, task.flexibility);
+    }
 
-        // // Check that flexibility is still zero
-        // assert_eq!(task.flexibility, 0);
+    /// Simulate a Task in test case bug_215 which is
+    /// for taking dineer daily basis
+    /// ```
+    ///     Task: Dinner,
+    ///     Duration: 1 hour
+    ///     Timing: daily between (6pm - 9pm)
+    /// ```
+    #[test]
+    fn test_task_dinner_time() {
+        let mut task = Task::mock(
+            1,
+            0,
+            TaskStatus::ReadyToSchedule,
+            vec![
+                Slot::mock(Duration::hours(3), 2023, 01, 03, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 04, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 05, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 06, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 07, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 08, 18, 0),
+                Slot::mock(Duration::hours(3), 2023, 01, 09, 18, 0),
+            ],
+        );
+        dbg!(&task);
 
-        // // Check that status is now Scheduled
-        // assert_eq!(task.status, TaskStatus::Scheduled);
+        task.calculate_flexibility();
+        dbg!(&task);
+
+        assert_eq!(21, task.flexibility);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_overlapped_tasks() {
+        todo!("test_overlapped_tasks");
+    }
+
+    #[test]
+    fn test_calc_flex() {
+        let task = Task::mock(
+            8,
+            0,
+            TaskStatus::ReadyToSchedule,
+            vec![
+                Slot::mock(Duration::hours(8), 2023, 01, 03, 0, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 03, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 04, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 05, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 06, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 07, 22, 0),
+                Slot::mock(Duration::hours(10), 2023, 01, 08, 22, 0),
+                Slot::mock(Duration::hours(2), 2023, 01, 09, 22, 0),
+            ],
+        );
+        dbg!(&task);
+
+        let flex = calc_flex(&task);
+        dbg!(flex);
+
+        assert_eq!(19, flex);
     }
 }
