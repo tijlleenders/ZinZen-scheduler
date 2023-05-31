06a75130a36c73774bb1a5e3cb56888eeba0c10d
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-05-31 03:30:07 +0300

    wip: - edge case test_sleep passed - calculate_flexibility edited to support force tasks respect min_duration test: organized tests for calculate_flexibility

--- src/services/flexibility.rs
+++ src/services/flexibility.rs
@@ -10,10 +10,13 @@ impl Task {
         let task_duration = self.duration;
         let flexibility = self.slots.iter().fold(0, |acc, slot| {
             let slot_duration = slot.duration_as_hours();
-            if slot_duration < task_duration {
-                acc + slot_duration
-            } else {
+            // TODO 2023-05-30  |   remove below condition "if slot_duration < task_duration"
+            // and just ask "if slot_duration >= task_duration"
+            // We can add partial condition when allow splitting min_duration tasks
+            if slot_duration >= task_duration {
                 acc + slot_duration - task_duration + 1
+            } else {
+                acc
             }
         });
 
@@ -23,29 +26,6 @@ impl Task {
 
         self.flexibility = flexibility;
     }
-}
-
-fn calc_flex(task: &Task) -> usize {
-    let first_slot_time = task.slots[0].start;
-    let start_time = first_slot_time;
-    let duration = task.duration;
-
-    let mut min_difference = start_time
-        .signed_duration_since(first_slot_time)
-        .num_hours();
-
-    for slot in task.slots.iter() {
-        dbg!(&slot);
-        let difference = slot.start.signed_duration_since(start_time).num_hours();
-
-        if difference < min_difference {
-            min_difference = difference;
-        }
-    }
-
-    let flex = min_difference as usize + duration;
-    dbg!(&flex);
-    flex
 }
 
 #[cfg(test)]
@@ -110,120 +90,150 @@ mod tests {
         todo!("test_status_budget_min_wait_adj");
     }
 
-    /// Simulate one Task in test case bug_215 which is Sleep
-    /// ```
-    ///     Task: Sleep,
-    ///     Duration: 8 hours
-    ///     Timing: 22-08
-    /// ```
-    #[test]
-    fn test_sleep_alone() {
-        let mut task = Task::mock(
-            8,
-            0,
-            TaskStatus::ReadyToSchedule,
-            vec![
-                Slot::mock(Duration::hours(8), 2023, 01, 03, 0, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 03, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 04, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 05, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 06, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 07, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 08, 22, 0),
-                // Slot::mock(Duration::hours(2), 2023, 01, 09, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 09, 22, 0),
-            ],
-        );
-        dbg!(&task);
+    mod single_tasks {
+        use chrono::Duration;
 
-        task.calculate_flexibility();
-        dbg!(&task);
+        use crate::models::{
+            slot::Slot,
+            task::{Task, TaskStatus},
+        };
 
-        assert_eq!(19, task.flexibility);
+        /// Simulate one Task in test case bug_215 which is Sleep
+        /// ```
+        ///     Task: Sleep,
+        ///     Duration: 8 hours
+        ///     Timing: 22-08
+        /// ```
+        #[test]
+        fn test_sleep() {
+            let mut task = Task::mock(
+                8,
+                0,
+                TaskStatus::ReadyToSchedule,
+                vec![
+                    Slot::mock(Duration::hours(8), 2023, 01, 03, 0, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 03, 22, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 04, 22, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 05, 22, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 06, 22, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 07, 22, 0),
+                    Slot::mock(Duration::hours(10), 2023, 01, 08, 22, 0),
+                    Slot::mock(Duration::hours(2), 2023, 01, 09, 22, 0),
+                    // Slot::mock(Duration::hours(10), 2023, 01, 09, 22, 0),
+                ],
+            );
+            dbg!(&task);
+
+            task.calculate_flexibility();
+            dbg!(&task);
+
+            assert_eq!(19, task.flexibility);
+        }
+
+        /// Simulate a Task in test case bug_215 which is
+        /// have highest flexibility because it can be assigned anytime
+        /// ```
+        ///     Task: Refreshing,
+        ///     Duration: 1 hour
+        ///     Timing: anytime
+        /// ```
+        #[test]
+        fn test_anytime_1hr() {
+            let mut task = Task::mock(
+                1,
+                0,
+                TaskStatus::ReadyToSchedule,
+                vec![Slot::mock(Duration::days(7), 2023, 01, 03, 0, 0)],
+            );
+            dbg!(&task);
+
+            task.calculate_flexibility();
+            dbg!(&task);
+
+            assert_eq!(168, task.flexibility);
+        }
+
+        /// Simulate a Task in test case bug_215 which is
+        /// for taking dineer daily basis
+        /// ```
+        ///     Task: Dinner,
+        ///     Duration: 1 hour
+        ///     Timing: daily between (6pm - 9pm)
+        /// ```
+        #[test]
+        fn test_dinner_time() {
+            let mut task = Task::mock(
+                1,
+                0,
+                TaskStatus::ReadyToSchedule,
+                vec![
+                    Slot::mock(Duration::hours(3), 2023, 01, 03, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 04, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 05, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 06, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 07, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 08, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 09, 18, 0),
+                ],
+            );
+            dbg!(&task);
+
+            task.calculate_flexibility();
+            dbg!(&task);
+
+            assert_eq!(21, task.flexibility);
+        }
     }
 
-    /// Simulate a Task in test case bug_215 which is
-    /// have highest flexibility because it can be assigned anytime
-    /// ```
-    ///     Task: Refreshing,
-    ///     Duration: 1 hour
-    ///     Timing: anytime
-    /// ```
-    #[test]
-    fn test_task_anytime_1hr() {
-        let mut task = Task::mock(
-            1,
-            0,
-            TaskStatus::ReadyToSchedule,
-            vec![Slot::mock(Duration::days(7), 2023, 01, 03, 0, 0)],
-        );
-        dbg!(&task);
+    mod multiple_tasks {
+        use chrono::Duration;
 
-        task.calculate_flexibility();
-        dbg!(&task);
+        use crate::models::{
+            slot::Slot,
+            task::{Task, TaskStatus},
+        };
 
-        assert_eq!(168, task.flexibility);
+        /// An edge case test which simulating 2 tasks and avail slots
+        ///  in the first day is less than min_duration for first task,
+        ///  so other task will be assigned in first instead.
+        /// ```
+        ///     Task 1: Sleep,
+        ///     Duration: 8 hours
+        ///     Timing: 22-08
+        ///
+        ///     Task 2: Thinking,
+        ///     Duration: 1 hour
+        ///     Timing: anytime
+        /// ```
+        #[test]
+        fn test_avail_slots_less_than_dur() {
+            let mut task = Task::mock(
+                1,
+                0,
+                TaskStatus::ReadyToSchedule,
+                vec![
+                    Slot::mock(Duration::hours(3), 2023, 01, 03, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 04, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 05, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 06, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 07, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 08, 18, 0),
+                    Slot::mock(Duration::hours(3), 2023, 01, 09, 18, 0),
+                ],
+            );
+            dbg!(&task);
+
+            task.calculate_flexibility();
+            dbg!(&task);
+
+            assert_eq!(21, task.flexibility);
+        }
     }
 
-    /// Simulate a Task in test case bug_215 which is
-    /// for taking dineer daily basis
-    /// ```
-    ///     Task: Dinner,
-    ///     Duration: 1 hour
-    ///     Timing: daily between (6pm - 9pm)
-    /// ```
-    #[test]
-    fn test_task_dinner_time() {
-        let mut task = Task::mock(
-            1,
-            0,
-            TaskStatus::ReadyToSchedule,
-            vec![
-                Slot::mock(Duration::hours(3), 2023, 01, 03, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 04, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 05, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 06, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 07, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 08, 18, 0),
-                Slot::mock(Duration::hours(3), 2023, 01, 09, 18, 0),
-            ],
-        );
-        dbg!(&task);
-
-        task.calculate_flexibility();
-        dbg!(&task);
-
-        assert_eq!(21, task.flexibility);
-    }
-
+    ///
     #[test]
     #[ignore]
     fn test_overlapped_tasks() {
         todo!("test_overlapped_tasks");
-    }
-
-    #[test]
-    fn test_calc_flex() {
-        let task = Task::mock(
-            8,
-            0,
-            TaskStatus::ReadyToSchedule,
-            vec![
-                Slot::mock(Duration::hours(8), 2023, 01, 03, 0, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 03, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 04, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 05, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 06, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 07, 22, 0),
-                Slot::mock(Duration::hours(10), 2023, 01, 08, 22, 0),
-                Slot::mock(Duration::hours(2), 2023, 01, 09, 22, 0),
-            ],
-        );
-        dbg!(&task);
-
-        let flex = calc_flex(&task);
-        dbg!(flex);
-
-        assert_eq!(19, flex);
     }
 }
