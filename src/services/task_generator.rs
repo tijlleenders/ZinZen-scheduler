66754893eb3201e5175bb834039888dde68016b6
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-06-21 17:44:18 +0300

    feat: developed function to generate tasks if exceeded 8 hours called NewTask::generate_tasks test: basic test cases passed

--- src/services/task_generator.rs
+++ src/services/task_generator.rs
@@ -3,8 +3,60 @@ use crate::models::slots_iterator::TimeSlotsIterator;
 use crate::models::task::{NewTask, Task, TaskStatus};
 use chrono::NaiveDateTime;
 
+impl NewTask {
+    /// Generate tasks based on a given NewTask and apply business rules
+    /// - Rule: task.duration must be greater than 1 and less than 8,
+    /// if more than  8, will be splitted
+    pub fn generate_tasks(&self) -> Vec<Task> {
+        let threshold: usize = 8;
+        if self.duration > 1 && self.duration < threshold {
+            let task = Task::new(self.clone());
+            dbg!(&task);
+            vec![task]
+        } else {
+            let mut total_duration: usize = self.duration;
+            let mut tasks: Vec<Task> = Vec::new();
+            // =========== First way to calculate new tasks
+            // - Deduct from total_duration 8 hours (or less for last task) each time until total_duration = 0
+            let mut counter = 0;
+            while total_duration > 0 {
+                let new_duration: usize;
+
+                if total_duration >= threshold {
+                    total_duration -= threshold;
+                    new_duration = threshold;
+                } else {
+                    new_duration = total_duration;
+                    total_duration = 0;
+                }
+
+                let mut new_task = self.clone();
+                new_task.duration = new_duration;
+                new_task.task_id += counter;
+                tasks.push(Task::new(new_task));
+                counter += 1;
+                dbg!(&tasks);
+            }
+
+            // =========== Another way to calculate new tasks
+            // // Used f32 to be able to get round up (ceil) for accurate calculation
+            // let tasks_count = (total_duration as f32 / 8.0).ceil() as usize;
+            // let mut tasks: Vec<Task> = Vec::new();
+
+            // for _ in 0..tasks_count {
+            //     let task = Task::new(self.clone());
+            //     dbg!(&task);
+            //     tasks.push(task);
+            // }
+
+            tasks
+        }
+    }
+}
+
 impl Task {
-    /// Create new task
+    /// Create new task based on NewTask object
+    /// - Note: in case you want to create new tasks based on business rule, kindly use NewTask::generate_tasks
     pub fn new(new_task: NewTask) -> Task {
         let start = new_task.timeframe.map(|time| time.start);
         let deadline = new_task.timeframe.map(|time| time.end);
@@ -88,5 +140,74 @@ impl Goal {
         }
         dbg!(&tasks);
         tasks
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    mod new_task {
+        use crate::models::{
+            goal::Goal,
+            slot::Slot,
+            task::{NewTask, Task, TaskStatus},
+            timeline::Timeline,
+        };
+        use chrono::Duration;
+
+        #[test]
+        fn test_duration_is_less_8_hrs() {
+            let duration: usize = 7;
+            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
+
+            let new_task = NewTask {
+                task_id: 1,
+                title: "test".to_string(),
+                duration,
+                goal: Goal::mock("1", "test", timeframe.clone()),
+                timeline: Timeline::new(),
+                status: TaskStatus::ReadyToSchedule,
+                timeframe: Some(timeframe),
+            };
+
+            let expected_task =
+                Task::mock("test", 7, 0, TaskStatus::ReadyToSchedule, vec![timeframe]);
+
+            let generated_task = new_task.generate_tasks();
+
+            assert_eq!(generated_task, vec![expected_task.clone()]);
+            assert_eq!(generated_task[0].id, expected_task.id);
+            assert_eq!(generated_task[0].duration, expected_task.duration);
+            assert_eq!(generated_task[0].status, expected_task.status);
+        }
+
+        #[test]
+        fn test_duration_is_more_8_hrs() {
+            let duration: usize = 10;
+
+            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
+
+            let new_task = NewTask {
+                task_id: 1,
+                title: "test".to_string(),
+                duration,
+                goal: Goal::mock("1", "test", timeframe.clone()),
+                timeline: Timeline::new(),
+                status: TaskStatus::ReadyToSchedule,
+                timeframe: Some(timeframe),
+            };
+            dbg!(&new_task);
+
+            let mut expected_task = vec![
+                Task::mock("test", 8, 0, TaskStatus::ReadyToSchedule, vec![timeframe]),
+                Task::mock("test", 2, 0, TaskStatus::ReadyToSchedule, vec![timeframe]),
+            ];
+            expected_task[1].id = 2;
+            dbg!(&expected_task);
+
+            let generated_task = new_task.generate_tasks();
+            dbg!(&generated_task);
+            // assert_eq!(generated_task, vec![expected_task]);
+        }
     }
 }
