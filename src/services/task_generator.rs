5f4a1813d2af88a7f21d141b55aeb13bf54adc3d
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-06-22 12:40:49 +0300

    ref: remove function NewTask::generate_tasks with it's test cases which useless at the moment

--- src/services/task_generator.rs
+++ src/services/task_generator.rs
@@ -4,46 +4,6 @@ use crate::models::slots_iterator::TimeSlotsIterator;
 use crate::models::task::{NewTask, Task, TaskStatus};
 use chrono::NaiveDateTime;
 
-impl NewTask {
-    /// Generate tasks based on a given NewTask and apply business rules
-    /// - Rule: task.duration must be greater than 1 and less than 8,
-    /// if more than  8, will be splitted
-    pub fn generate_tasks(&self) -> Vec<Task> {
-        let threshold: usize = 8;
-        if self.duration > 1 && self.duration < threshold {
-            let task = Task::new(self.clone());
-            dbg!(&task);
-            vec![task]
-        } else {
-            let mut total_duration: usize = self.duration;
-            let mut tasks: Vec<Task> = Vec::new();
-            // =========== First way to calculate new tasks
-            // - Deduct from total_duration 8 hours (or less for last task) each time until total_duration = 0
-            let mut counter = 0;
-            while total_duration > 0 {
-                let new_duration: usize;
-
-                if total_duration >= threshold {
-                    total_duration -= threshold;
-                    new_duration = threshold;
-                } else {
-                    new_duration = total_duration;
-                    total_duration = 0;
-                }
-
-                let mut new_task = self.clone();
-                new_task.duration = new_duration;
-                new_task.task_id += counter;
-                tasks.push(Task::new(new_task));
-                counter += 1;
-                dbg!(&tasks);
-            }
-
-            tasks
-        }
-    }
-}
-
 impl Task {
     /// Create new task based on NewTask object
     pub fn new(new_task: NewTask) -> Task {
@@ -215,303 +175,208 @@ impl Goal {
 #[cfg(test)]
 mod tests {
 
-    mod apply_duration_threshold {
-        use chrono::Duration;
-
-        use crate::models::{
-            goal::Goal,
-            slot::Slot,
-            task::{NewTask, Task, TaskStatus},
-            timeline::Timeline,
-        };
-
-        #[test]
-        fn test_duration_less_8_hrs() {
-            let duration: usize = 7;
-            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
-
-            let new_task = NewTask {
-                task_id: 1,
-                title: "test".to_string(),
-                duration,
-                goal: Goal::mock("1", "test", timeframe.clone()),
-                timeline: Timeline::new(),
-                status: TaskStatus::ReadyToSchedule,
-                timeframe: Some(timeframe),
-            };
-            let new_task = Task::new(new_task);
-            dbg!(&new_task);
-
-            let generated_tasks = new_task.apply_duration_threshold();
-            dbg!(&generated_tasks);
-
-            let expected_task = Task::mock(
-                "test",
-                7,
-                0,
-                TaskStatus::ReadyToSchedule,
-                vec![timeframe],
-                None,
-            );
-
-            assert_eq!(generated_tasks, vec![expected_task.clone()]);
-            assert_eq!(generated_tasks[0].id, expected_task.id);
-            assert_eq!(generated_tasks[0].duration, expected_task.duration);
-            assert_eq!(generated_tasks[0].status, expected_task.status);
-        }
-
-        /// Test Task::apply_duration_threshold when goal.min_duration > 8 hours
-        /// ```markdown
-        /// =========================
-        /// Input:
-        /// Goal {
-        ///    id: "1",
-        ///    title: "test",
-        ///    min_duration: Some(
-        ///        10,
-        ///    ),
-        ///    max_duration: None,
-        ///    budgets: None,
-        ///    repeat: None,
-        ///    start: Some(
-        ///        2023-06-01T00:00:00,
-        ///    ),
-        ///    deadline: Some(
-        ///        2023-06-06T00:00:00,
-        ///    ),
-        ///    tags: [],
-        ///    filters: None,
-        ///    children: None,
-        ///    after_goals: None,
-        ///}
-        ///
-        /// ===========================
-        /// Output:
-        /// expected_task = [
-        ///    Task {
-        ///        id: 1,
-        ///        goal_id: "1",
-        ///        title: "test",
-        ///        duration: 8,
-        ///        status: ReadyToSchedule,
-        ///        flexibility: 0,
-        ///        start: None,
-        ///        deadline: None,
-        ///        slots: [
-        ///            Slot {
-        ///                start:   2023-06-01 00,
-        ///                 end:    2023-06-06 00,
-        ///            },
-        ///        ],
-        ///        tags: [],
-        ///        after_goals: None,
-        ///    },
-        ///    Task {
-        ///        id: 2,
-        ///        goal_id: "1",
-        ///        title: "test",
-        ///        duration: 1,
-        ///        status: ReadyToSchedule,
-        ///        flexibility: 0,
-        ///        start: None,
-        ///        deadline: None,
-        ///        slots: [
-        ///            Slot {
-        ///                start:   2023-06-01 00,
-        ///                 end:    2023-06-06 00,
-        ///            },
-        ///        ],
-        ///        tags: [],
-        ///        after_goals: None,
-        ///    },
-        ///    Task {
-        ///        id: 3,
-        ///        goal_id: "1",
-        ///        title: "test",
-        ///        duration: 1,
-        ///        status: ReadyToSchedule,
-        ///        flexibility: 0,
-        ///        start: None,
-        ///        deadline: None,
-        ///        slots: [
-        ///            Slot {
-        ///                start:   2023-06-01 00,
-        ///                 end:    2023-06-06 00,
-        ///            },
-        ///        ],
-        ///        tags: [],
-        ///        after_goals: None,
-        ///    },
-        ///
-        ///]
-        ///
-        ///
-        /// ```
-        #[test]
-        fn test_duration_more_8_hrs() {
-            let duration: usize = 10;
-            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
-            let timeline = Timeline {
-                slots: vec![timeframe.clone()].into_iter().collect(),
-            };
-
-            let new_task = NewTask {
-                task_id: 1,
-                title: "test".to_string(),
-                duration,
-                goal: Goal::mock("1", "test", timeframe.clone()),
-                timeline,
-                status: TaskStatus::ReadyToSchedule,
-                timeframe: None,
-            };
-            let new_task = Task::new(new_task);
-            dbg!(&new_task);
-            let generated_tasks = new_task.apply_duration_threshold();
-            dbg!(&generated_tasks);
-
-            let mut expected_task = vec![
-                Task::mock(
-                    "test",
-                    8,
-                    0,
-                    TaskStatus::ReadyToSchedule,
-                    vec![timeframe],
-                    None,
-                ),
-                Task::mock(
-                    "test",
-                    1,
-                    0,
-                    TaskStatus::ReadyToSchedule,
-                    vec![timeframe],
-                    None,
-                ),
-                Task::mock(
-                    "test",
-                    1,
-                    0,
-                    TaskStatus::ReadyToSchedule,
-                    vec![timeframe],
-                    None,
-                ),
-            ];
-            expected_task[1].id = 2;
-            expected_task[2].id = 3;
-            dbg!(&expected_task);
-
-            assert_eq!(generated_tasks, expected_task);
-            assert_eq!(generated_tasks.len(), 3);
-
-            assert_eq!(generated_tasks[0].id, expected_task[0].id);
-            assert_eq!(generated_tasks[1].id, expected_task[1].id);
-            assert_eq!(generated_tasks[2].id, expected_task[2].id);
-
-            assert_eq!(generated_tasks[0].duration, expected_task[0].duration);
-            assert_eq!(generated_tasks[1].duration, expected_task[1].duration);
-            assert_eq!(generated_tasks[2].duration, expected_task[2].duration);
-
-            assert_eq!(generated_tasks[0].status, expected_task[0].status);
-            assert_eq!(generated_tasks[1].status, expected_task[1].status);
-            assert_eq!(generated_tasks[2].status, expected_task[2].status);
-        }
-    }
-
-    mod new_task {
-        use crate::models::{
-            goal::Goal,
-            slot::Slot,
-            task::{NewTask, Task, TaskStatus},
-            timeline::Timeline,
-        };
-        use chrono::Duration;
-
-        #[test]
-        fn test_duration_is_less_8_hrs() {
-            let duration: usize = 7;
-            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
-
-            let new_task = NewTask {
-                task_id: 1,
-                title: "test".to_string(),
-                duration,
-                goal: Goal::mock("1", "test", timeframe.clone()),
-                timeline: Timeline::new(),
-                status: TaskStatus::ReadyToSchedule,
-                timeframe: Some(timeframe),
-            };
-
-            let expected_task = Task::mock(
-                "test",
-                7,
-                0,
-                TaskStatus::ReadyToSchedule,
-                vec![timeframe],
-                None,
-            );
-
-            let generated_task = new_task.generate_tasks();
-
-            assert_eq!(generated_task, vec![expected_task.clone()]);
-            assert_eq!(generated_task[0].id, expected_task.id);
-            assert_eq!(generated_task[0].duration, expected_task.duration);
-            assert_eq!(generated_task[0].status, expected_task.status);
-        }
-
-        #[test]
-        fn test_duration_is_more_8_hrs() {
-            let duration: usize = 10;
-
-            let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
-
-            let new_task = NewTask {
-                task_id: 1,
-                title: "test".to_string(),
-                duration,
-                goal: Goal::mock("1", "test", timeframe.clone()),
-                timeline: Timeline::new(),
-                status: TaskStatus::ReadyToSchedule,
-                timeframe: Some(timeframe),
-            };
-            dbg!(&new_task);
-
-            let mut expected_tasks = vec![
-                Task::mock(
-                    "test",
-                    8,
-                    0,
-                    TaskStatus::ReadyToSchedule,
-                    vec![timeframe],
-                    None,
-                ),
-                Task::mock(
-                    "test",
-                    2,
-                    0,
-                    TaskStatus::ReadyToSchedule,
-                    vec![timeframe],
-                    None,
-                ),
-            ];
-            expected_tasks[1].id = 2;
-            dbg!(&expected_tasks);
-
-            let tasks = new_task.generate_tasks();
-            dbg!(&tasks);
-
-            assert_eq!(tasks, expected_tasks);
-
-            assert_eq!(tasks[0].id, expected_tasks[0].id);
-            assert_eq!(tasks[1].id, expected_tasks[1].id);
-
-            assert_eq!(tasks[0].duration, expected_tasks[0].duration);
-            assert_eq!(tasks[1].duration, expected_tasks[1].duration);
-
-            assert_eq!(tasks[0].status, expected_tasks[0].status);
-            assert_eq!(tasks[1].status, expected_tasks[1].status);
-        }
-    }
-
     mod goal {
+        mod apply_duration_threshold {
+            use chrono::Duration;
+
+            use crate::models::{
+                goal::Goal,
+                slot::Slot,
+                task::{NewTask, Task, TaskStatus},
+                timeline::Timeline,
+            };
+
+            #[test]
+            fn test_duration_less_8_hrs() {
+                let duration: usize = 7;
+                let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
+
+                let new_task = NewTask {
+                    task_id: 1,
+                    title: "test".to_string(),
+                    duration,
+                    goal: Goal::mock("1", "test", timeframe.clone()),
+                    timeline: Timeline::new(),
+                    status: TaskStatus::ReadyToSchedule,
+                    timeframe: Some(timeframe),
+                };
+                let new_task = Task::new(new_task);
+                dbg!(&new_task);
+
+                let generated_tasks = new_task.apply_duration_threshold();
+                dbg!(&generated_tasks);
+
+                let expected_task = Task::mock(
+                    "test",
+                    7,
+                    0,
+                    TaskStatus::ReadyToSchedule,
+                    vec![timeframe],
+                    None,
+                );
+
+                assert_eq!(generated_tasks, vec![expected_task.clone()]);
+                assert_eq!(generated_tasks[0].id, expected_task.id);
+                assert_eq!(generated_tasks[0].duration, expected_task.duration);
+                assert_eq!(generated_tasks[0].status, expected_task.status);
+            }
+
+            /// Test Task::apply_duration_threshold when goal.min_duration > 8 hours
+            /// ```markdown
+            /// =========================
+            /// Input:
+            /// Goal {
+            ///    id: "1",
+            ///    title: "test",
+            ///    min_duration: Some(
+            ///        10,
+            ///    ),
+            ///    max_duration: None,
+            ///    budgets: None,
+            ///    repeat: None,
+            ///    start: Some(
+            ///        2023-06-01T00:00:00,
+            ///    ),
+            ///    deadline: Some(
+            ///        2023-06-06T00:00:00,
+            ///    ),
+            ///    tags: [],
+            ///    filters: None,
+            ///    children: None,
+            ///    after_goals: None,
+            ///}
+            ///
+            /// ===========================
+            /// Output:
+            /// expected_task = [
+            ///    Task {
+            ///        id: 1,
+            ///        goal_id: "1",
+            ///        title: "test",
+            ///        duration: 8,
+            ///        status: ReadyToSchedule,
+            ///        flexibility: 0,
+            ///        start: None,
+            ///        deadline: None,
+            ///        slots: [
+            ///            Slot {
+            ///                start:   2023-06-01 00,
+            ///                 end:    2023-06-06 00,
+            ///            },
+            ///        ],
+            ///        tags: [],
+            ///        after_goals: None,
+            ///    },
+            ///    Task {
+            ///        id: 2,
+            ///        goal_id: "1",
+            ///        title: "test",
+            ///        duration: 1,
+            ///        status: ReadyToSchedule,
+            ///        flexibility: 0,
+            ///        start: None,
+            ///        deadline: None,
+            ///        slots: [
+            ///            Slot {
+            ///                start:   2023-06-01 00,
+            ///                 end:    2023-06-06 00,
+            ///            },
+            ///        ],
+            ///        tags: [],
+            ///        after_goals: None,
+            ///    },
+            ///    Task {
+            ///        id: 3,
+            ///        goal_id: "1",
+            ///        title: "test",
+            ///        duration: 1,
+            ///        status: ReadyToSchedule,
+            ///        flexibility: 0,
+            ///        start: None,
+            ///        deadline: None,
+            ///        slots: [
+            ///            Slot {
+            ///                start:   2023-06-01 00,
+            ///                 end:    2023-06-06 00,
+            ///            },
+            ///        ],
+            ///        tags: [],
+            ///        after_goals: None,
+            ///    },
+            ///
+            ///]
+            ///
+            ///
+            /// ```
+            #[test]
+            fn test_duration_more_8_hrs() {
+                let duration: usize = 10;
+                let timeframe = Slot::mock(Duration::days(5), 2023, 6, 1, 0, 0);
+                let timeline = Timeline {
+                    slots: vec![timeframe.clone()].into_iter().collect(),
+                };
+
+                let new_task = NewTask {
+                    task_id: 1,
+                    title: "test".to_string(),
+                    duration,
+                    goal: Goal::mock("1", "test", timeframe.clone()),
+                    timeline,
+                    status: TaskStatus::ReadyToSchedule,
+                    timeframe: None,
+                };
+                let new_task = Task::new(new_task);
+                dbg!(&new_task);
+                let generated_tasks = new_task.apply_duration_threshold();
+                dbg!(&generated_tasks);
+
+                let mut expected_task = vec![
+                    Task::mock(
+                        "test",
+                        8,
+                        0,
+                        TaskStatus::ReadyToSchedule,
+                        vec![timeframe],
+                        None,
+                    ),
+                    Task::mock(
+                        "test",
+                        1,
+                        0,
+                        TaskStatus::ReadyToSchedule,
+                        vec![timeframe],
+                        None,
+                    ),
+                    Task::mock(
+                        "test",
+                        1,
+                        0,
+                        TaskStatus::ReadyToSchedule,
+                        vec![timeframe],
+                        None,
+                    ),
+                ];
+                expected_task[1].id = 2;
+                expected_task[2].id = 3;
+                dbg!(&expected_task);
+
+                assert_eq!(generated_tasks, expected_task);
+                assert_eq!(generated_tasks.len(), 3);
+
+                assert_eq!(generated_tasks[0].id, expected_task[0].id);
+                assert_eq!(generated_tasks[1].id, expected_task[1].id);
+                assert_eq!(generated_tasks[2].id, expected_task[2].id);
+
+                assert_eq!(generated_tasks[0].duration, expected_task[0].duration);
+                assert_eq!(generated_tasks[1].duration, expected_task[1].duration);
+                assert_eq!(generated_tasks[2].duration, expected_task[2].duration);
+
+                assert_eq!(generated_tasks[0].status, expected_task[0].status);
+                assert_eq!(generated_tasks[1].status, expected_task[1].status);
+                assert_eq!(generated_tasks[2].status, expected_task[2].status);
+            }
+        }
+
         mod generate_tasks {
             use chrono::Duration;
 
