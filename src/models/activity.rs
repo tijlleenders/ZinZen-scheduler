7eedc103a99f89cfebc4b5d878f2055a3fe6b605
Tijl Leenders <gh@zinzen.com>
2024-01-07 15:39:12 +0100

    simplify generating activities from budget goal

--- src/models/activity.rs
+++ src/models/activity.rs
@@ -201,27 +201,12 @@ fn get_activities_from_budget_goal(goal: Goal, calendar: &Calendar) -> Vec<Activ
     //This is to not cut something like Sleep into pieces
     //Maybe better replaced by an if on title == 'Sleep'?
     //Is the default case that you allow splitting OK?
-    let mut activity_total_duration = 1;
-    match goal.min_duration.clone() {
-        Some(min_duration) => {
-            activity_total_duration = min_duration;
-        }
-        None => {
-            activity_total_duration = goal.budget_config.unwrap().min_per_day;
-        }
-    }
-    let mut min_block_size = activity_total_duration;
-    if activity_total_duration > 8 {
-        min_block_size = 1;
-        todo!() //split into multiple activities so flexibilities are correct??
-                // or yield flex 1 or maximum of the set from activity.flex()?
-    };
+    let activity_total_duration = goal.budget_config.unwrap().min_per_day;
 
-    let mut number_of_activites = 1;
-    number_of_activites = (adjusted_goal_deadline - adjusted_goal_start).num_days() as u64 + 1;
-    println!("num_activities: {:?}", &number_of_activites);
-    adjusted_goal_deadline = adjusted_goal_start.add(Days::new(number_of_activites));
-    for _ in 0..number_of_activites {
+    let number_of_activities = (adjusted_goal_deadline - adjusted_goal_start).num_days() as u64 + 1;
+    println!("num_activities: {:?}", &number_of_activities);
+    adjusted_goal_deadline = adjusted_goal_start.add(Days::new(number_of_activities));
+    for _ in 0..number_of_activities {
         let compatible_hours_overlay = Activity::get_compatible_hours_overlay(
             &calendar,
             Some(filter_option.clone()),
@@ -232,12 +217,12 @@ fn get_activities_from_budget_goal(goal: Goal, calendar: &Calendar) -> Vec<Activ
         let activity = Activity {
             id: goal.id.clone(),
             title: goal.title.clone(),
-            min_block_size,
-            max_block_size: min_block_size,
+            min_block_size: 1,
+            max_block_size: 1,
             calendar_overlay: compatible_hours_overlay,
             budget: None,
             total_duration: activity_total_duration,
-            duration_left: min_block_size, //TODO: Correct this - is it even necessary to have duration_left?
+            duration_left: 0, //TODO: Correct this - is it even necessary to have duration_left?
             status: Status::Unprocessed,
         };
         dbg!(&activity);
