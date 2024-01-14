738c8f037380ec2b9b81585c94e2e4aa483157a2
Tijl Leenders <gh@zinzen.com>
2024-01-14 19:44:14 +0100

    cleanup comments

--- src/models/budget.rs
+++ src/models/budget.rs
@@ -4,17 +4,6 @@ use serde::Deserialize;
 
 use super::{calendar::Calendar, goal::Goal};
 
-//Can a budget have other budgets?
-
-//Budget per day/week in one budget?
-//   NO, separate list of budget per period (each day and each week) - per goal-id
-//remove datetime completely? YES
-
-// check with budget should be on the whole range of the block, taking into account the min_block
-
-//activity has budgets it belongs to, placer checks each budget ...
-//   NO, flex should do that and remove any that are not allowed
-// ... is that enough ... or do we need an extra check on placing?
 #[derive(Debug, Clone, Deserialize)]
 pub struct Budget {
     pub originating_goal_id: String,
