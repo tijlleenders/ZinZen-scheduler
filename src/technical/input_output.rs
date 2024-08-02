5f5fae736952447a3aafb5171c315eaa62b03137
tijlleenders <gh@zinzen.me>
2024-08-02 11:15:21 +0200

    fix API contract

--- src/technical/input_output.rs
+++ src/technical/input_output.rs
@@ -8,6 +8,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use crate::models::goal::Slot;
 
 #[derive(Deserialize, Debug)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
@@ -16,12 +17,13 @@ pub struct Input {
     pub end_date: NaiveDateTime,
     pub goals: Vec<Goal>,
     pub tasks_completed_today: Vec<TaskCompletedToday>,
+    pub global_not_on: Option<Vec<Slot>>,
 }
 
 pub fn get_input_from_json<P: AsRef<Path>>(path: P) -> Result<Input, Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-    let input = serde_json::from_reader(reader)?;
+    let input= serde_json::from_reader(reader)?;
     Ok(input)
 }
 
